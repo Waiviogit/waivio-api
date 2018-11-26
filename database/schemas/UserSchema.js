e249528ae1f33ca64e597f9bbcce80b6cfcbeae3
Maxim Oancha <maksimoancha12@gmail.com>
2018-11-26 12:13:29 +0200

    changed database schemas to new specifics

--- database/schemas/UserSchema.js
+++ database/schemas/UserSchema.js
@@ -5,7 +5,7 @@ const UserSchema = new Schema({
     name: {type: String, index: true, unique: true},
     profile_image: String,
     wObjects:[{
-        tag:String,
+        authorPermlink:String,
         weight: Number,  //Object Shares, value in STEEM(or WVIO) coin
         rank: Number     //Object Expertise, value from 1 to 99
     }],
