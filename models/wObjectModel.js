1e7868c8a0a1ed15bad101ebe89ca3952da60b01
Maxim Oancha <maksimoancha12@gmail.com>
2018-11-28 10:51:54 +0200

    edit formatting fields for show wobject, all wobject and for search

--- models/wObjectModel.js
+++ models/wObjectModel.js
@@ -13,20 +13,26 @@ const create = async function (data) {
 const search = async function (data) {
     try {
         const wObjects = await WObjectModel
-            .find({'fields':
+            .find({
+                'fields':
                     {
-                        $elemMatch:{
-                            'name':'name',
-                            'body':{$regex: `^${data.string}`, $options: 'i'}
+                        $elemMatch: {
+                            'name': 'name',
+                            'body': {$regex: `^${data.string}`, $options: 'i'}
                         }
                     }
             })
-            .sort({weight:-1})
+            .sort({weight: -1})
             .limit(data.limit)
             .select('-_id -fields._id')
             .lean();
+        const requireFields = [
+            {name: 'avatarImage'},
+            {name: 'name'},
+            {name: 'link'},
+            {name: 'locationCity'}];
         wObjects.forEach((wObject) => {
-            formatFields(wObject, data.locale);
+            formatFields(wObject, data.locale, requireFields);
         });
 
         return {wObjectsData: wObjects};
@@ -44,7 +50,26 @@ const getOne = async function (data) {      //get one wobject by authorPermlink
             .lean();
         formatUsers(wObject);
         wObject.followersCount = wObject.followersNames ? wObject.followersNames.length : 0;
-
+        const requireFields = [
+            {name: 'name'},
+            {name: 'descriptionShort'},
+            {name: 'descriptionFull'},
+            {name: 'locationCountry'},
+            {name: 'locationCity'},
+            {name: 'locationStreet'},
+            {name: 'locationAccomodation'},
+            {name: 'locationGPS'},
+            {name: 'postCode'},
+            {name: 'link'},
+            {name: 'linkFacebook'},
+            {name: 'linkTwitter'},
+            {name: 'linkYoutube'},
+            {name: 'linkInstagram'},
+            {name: 'linkVk'},
+            {name: 'avatarImage'},
+            {name: 'backgroundImage'}
+        ];
+        formatFields(wObject, data.locale, requireFields);
         return {wObjectData: wObject};
     } catch (error) {
         return {error};
@@ -63,12 +88,17 @@ const getAll = async function (data) {
         const beginIndex = data.startAuthorPermlink ? wObjects.map(item => item.authorPermlink).indexOf(data.startAuthorPermlink) + 1 : 0;
         wObjects = wObjects.slice(beginIndex, beginIndex + data.limit);
 
+        const requireFields = [
+            {name: 'avatarImage'},
+            {name: 'name'},
+            {name: 'link'},
+            {name: 'locationCity'}];
         wObjects.forEach((wObject) => {
             formatUsers(wObject);
             wObject.children = wObject.children.map(item => item.authorPermlink);  //correct format of children
             wObject.user_count = wObject.users.length;                  //add field user count
             wObject.users = wObject.users.filter((item, index) => index < data.userLimit);
-            formatFields(wObject, data.locale);
+            formatFields(wObject, data.locale, requireFields);
         });
 
         return {wObjectsData: wObjects};
@@ -91,8 +121,7 @@ const formatUsers = function (wObject) {
     wObject.users = _.orderBy(wObject.users, ['rank'], ['desc']);  //order users by rank
 };
 
-const formatFields = function (wObject, locale) {
-    const requireFields = [{name: 'avatarImage'}, {name: 'name'}, {name: 'link'}, {name: 'locationCity'}];
+const formatFields = function (wObject, locale, requireFields) {
     const temp = _.reduce(wObject.fields, (resArr, field) => {
         const currResField = resArr.find(item => item.name === field.name);
         if (currResField && (!currResField.weight || currResField.weight < field.weight)) {
@@ -112,5 +141,5 @@ const formatFields = function (wObject, locale) {
         }
         return resArr;
     }, temp);
-};    // get best fields(avatarImage, name, location and link) in location, or just best field if is not field in location
+};    // get best fields(avatarImage, name, location and link) in location, or just best field if is have no field in locale
 module.exports = {create, getAll, getOne, search};
