29ea07e5d0664e307704440b36f66d91f7730844
Maxim Oancha <maksimoancha12@gmail.com>
2018-12-07 15:12:23 +0200

    add following objects request to user

--- database/schemas/UserSchema.js
+++ database/schemas/UserSchema.js
@@ -12,6 +12,14 @@ const UserSchema = new Schema({
     read_locales: [String],
     objects_follow: {type: [String], default: []} //arr of author_permlink of objects what user following
 }, {timestamps: true});
+
+UserSchema.virtual('full_objects_follow', { //get full structure of objects instead only author_permlink
+    ref: 'wobject',
+    localField: 'objects_follow',
+    foreignField: 'author_permlink',
+    justOne: false
+});
+
 const UserModel = mongoose.model('User', UserSchema);
 
 module.exports = UserModel;
