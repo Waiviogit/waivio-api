5304e78ea2398b9576cd5879b300ef8ef5ae022f
Maxim Oancha <maksimoancha12@gmail.com>
2019-03-22 12:09:36 +0200

    fix

--- utilities/helpers/userFeedHelper.js
+++ utilities/helpers/userFeedHelper.js
@@ -8,17 +8,18 @@ const getCombinedFeed = async function ({user, limit, count_with_wobj, start_aut
     const from_wobj_follow = await feedByObjects({user, limit, skip: count_with_wobj}); //posts from db
     if (!from_wobj_follow || from_wobj_follow.error)
         return {error: from_wobj_follow.error};
-    const from_user_follow = await postsUtil.getPostsByCategory({
+    let from_user_follow = await postsUtil.getPostsByCategory({
         category: 'feed',
         tag: user,
-        limit,
+        limit: limit + 1,
         start_author: start_author,
         start_permlink: start_permlink
     });                                                                                 //posts from blockchain
+    from_user_follow.posts = from_user_follow.posts.slice(1);
     for (const post of from_user_follow.posts) {          //for each post from user following add wobjects if it exist
         const postWobjects = await getPostObjects(post.author, post.permlink);
         if (postWobjects)
-            from_user_follow.posts.wobjects = postWobjects;
+            from_user_follow.post.wobjects = postWobjects; //??????????? post.wobjects = postWobjects;
     }
     if (!from_user_follow || from_user_follow.error)
         return {error: from_user_follow.error};
@@ -26,8 +27,15 @@ const getCombinedFeed = async function ({user, limit, count_with_wobj, start_aut
         (x, y) => x.author === y.author && x.permlink === y.permlink);
     combined_feed = _.orderBy(combined_feed, ['created'], ['desc']);
     combined_feed = combined_feed.slice(0, limit);
-    count_with_wobj += _.countBy(combined_feed, (post) => !!post._id).true || 0;
-    const last_from_user_follow = _.findLast(combined_feed, (p) => !!p.post_id);
+
+    let tempCombFeed = _.orderBy([...from_wobj_follow.posts, ...from_user_follow.posts], ['created'], ['desc']);
+    tempCombFeed = tempCombFeed.slice(0, _.findLastIndex(tempCombFeed, {
+        'author': _.last(combined_feed).author,
+        'permlink': _.last(combined_feed).permlink
+    }) + 1);
+
+    count_with_wobj += _.countBy(tempCombFeed, (post) => !!post._id).true || 0;
+    const last_from_user_follow = _.findLast(tempCombFeed, (p) => !!p.post_id);
     if (last_from_user_follow) {
         start_author = last_from_user_follow.author;
         start_permlink = last_from_user_follow.permlink;
