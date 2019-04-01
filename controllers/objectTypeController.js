d5deb4798ca918db0be4458ad6cb6b90622332c1
Maxim Oancha <maksimoancha12@gmail.com>
2019-04-01 13:58:53 +0300

    add merge top wobjects to objectsTypes

--- controllers/objectTypeController.js
+++ controllers/objectTypeController.js
@@ -3,7 +3,8 @@ const {ObjectType} = require('../models');
 const index = async (req, res, next) => {
     const {objectTypes, error} = await ObjectType.getAll({
         limit: req.body.limit || 30,
-        skip: req.body.skip || 0
+        skip: req.body.skip || 0,
+        wobjects_count: req.body.wobjects_count || 3
     });
     if (error)
         return next(error);
