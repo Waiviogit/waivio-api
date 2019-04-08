8c3ce4e51821142c0076a8424482d8d8590dcc06
Maxim <maksimoancha12@gmail.com>
2019-04-08 15:26:58 +0300

    add wobjects and body of post to objectType

--- controllers/objectTypeController.js
+++ controllers/objectTypeController.js
@@ -1,34 +1,38 @@
-const {ObjectType} = require('../models');
+const { ObjectType } = require('../models')
 
-const index = async (req, res, next) => {
-    const {objectTypes, error} = await ObjectType.getAll({
-        limit: req.body.limit || 30,
-        skip: req.body.skip || 0,
-        wobjects_count: req.body.wobjects_count || 3
-    });
-    if (error)
-        return next(error);
-    res.status(200).json(objectTypes);
-};
+const index = async ( req, res, next ) => {
+	const { objectTypes, error } = await ObjectType.getAll({
+		limit: req.body.limit || 30,
+		skip: req.body.skip || 0,
+		wobjects_count: req.body.wobjects_count || 3,
+	})
+	if(error) {
+		return next(error)
+	}
+	res.status(200).json(objectTypes)
+}
 
-const show = async (req, res, next) => {
-    const {objectType, error} = await ObjectType.getOne({name: req.params.objectTypeName});
-    if (error) {
-        return next(error);
-    }
-    res.status(200).json(objectType);
-};
+const show = async ( req, res, next ) => {
+	const { objectType, error } = await ObjectType.getOne({
+		name: req.params.objectTypeName,
+		wobjects_count: Number(req.query.wobjects_count) || 3,
+	})
+	if(error) {
+		return next(error)
+	}
+	res.status(200).json(objectType)
+}
 
-const search = async (req, res, next) => {
-    const {objectTypes, error} = await ObjectType.search({
-        string: req.body.search_string,
-        skip: req.body.skip || 0,
-        limit: req.body.limit || 30
-    });
-    if (error) {
-        return next(error);
-    }
-    res.status(200).json(objectTypes);
-};
+const search = async ( req, res, next ) => {
+	const { objectTypes, error } = await ObjectType.search({
+		string: req.body.search_string,
+		skip: req.body.skip || 0,
+		limit: req.body.limit || 30,
+	})
+	if(error) {
+		return next(error)
+	}
+	res.status(200).json(objectTypes)
+}
 
-module.exports = {index, search, show}
+module.exports = { index, search, show }
--- models/ObjectTypeModel.js
+++ models/ObjectTypeModel.js
@@ -1,5 +1,6 @@
 const {ObjectType} = require('../database').models;
 const {REQUIREDFIELDS} = require('../utilities/constants');
+const {postsUtil} = require('../utilities/steemApi');
 
 const getAll = async ({limit, skip, wobjects_count = 3}) => {
     let objectTypes
@@ -66,11 +67,52 @@ const search = async ({string, limit, skip}) => {
     }
 };
 
-const getOne = async ({name}) => {
+const getOne = async ({name, wobjects_count = 3}) => {
     try {
-        const objectType = await ObjectType.findOne({name: name}).lean();
+        // const objectType = await ObjectType.findOne({name: name}).lean();
+        const [objectType] = await ObjectType.aggregate([
+            { $match: { name } },
+            {
+                $lookup: {
+                    from: 'wobjects',
+                    as: 'related_wobjects',
+                    let: { object_type_name: '$name' },
+                    pipeline: [
+                        {
+                            $match: {
+                                $expr: { $eq: ['$object_type', '$$object_type_name'] },
+                            },
+                        },
+                        { $sort: { weight: -1 } },
+                        { $limit: wobjects_count + 1 },
+                        {
+                            $addFields: {
+                                'fields':{
+                                    $filter:{
+                                        input: '$fields',
+                                        as:'field',
+                                        cond:{
+                                            $in:['$$field.name', REQUIREDFIELDS]
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    ]
+                }
+            }
+        ])
+
         if (!objectType) {
             throw {status: 404, message: 'Object Type not found!'}
+        }
+        if(objectType.related_wobjects.length === wobjects_count + 1){
+            objectType.hasMoreWobjects = true;
+            objectType.related_wobjects = objectType.related_wobjects.slice(0,wobjects_count)
+        }
+        const {post} = await postsUtil.getPost(objectType.author, objectType.permlink)
+        if(post && post.body){
+            objectType.body = post.body;
         }
         return {objectType}
     } catch (e) {
