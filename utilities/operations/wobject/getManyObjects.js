f2c6b80ee8200d8f1b592d099606d991736439e1
Maxim Oancha <maksimoancha12@gmail.com>
2019-06-13 12:53:52 +0300

    add sample param to list of wobjects
    if sample true => get random 5 wobjects from top 100, move a lot of logics to operation

---
+++ utilities/operations/wobject/getManyObjects.js
@@ -0,0 +1,56 @@
+const { Wobj } = require( '../../../models' );
+const wObjectHelper = require( '../../helpers/wObjectHelper' );
+const UserWobjectsModel = require( '../../../database' ).models.UserWobjects;
+const { REQUIREFIELDS_PARENT } = require( '../../constants' );
+const rankHelper = require( '../../helpers/rankHelper' );
+const _ = require( 'lodash' );
+
+const getMany = async ( data ) => {
+    const { wObjectsData: wObjects, hasMore, error } = await Wobj.getAll( data );
+
+    if( error ) {
+        return { error };
+    }
+    if( data.user_limit ) {
+        await Promise.all( wObjects.map( async ( wobject ) => {
+            wobject.users = await UserWobjectsModel.aggregate( [
+                { $match: { author_permlink: wobject.author_permlink } },
+                { $sort: { weight: -1 } },
+                { $limit: data.user_limit },
+                {
+                    $project: {
+                        _id: 0,
+                        name: '$user_name',
+                        weight: 1
+                    }
+                }
+            ] );
+        } ) );
+    } // assign top users to each of wobject
+
+    wObjects.forEach( ( wObject ) => {
+        wObject.users = wObject.users || [];
+        wObject.user_count = wObject.users.length; // add field user count
+        // wObjectHelper.formatRequireFields( wObject, data.locale, required_fields.map( ( item ) => ( { name: item } ) ) );
+
+        // format parent field
+        if( Array.isArray( wObject.parent ) ) {
+            if( _.isEmpty( wObject.parent ) ) {
+                wObject.parent = '';
+            } else {
+                wObject.parent = wObject.parent[ 0 ];
+                getRequiredFields( wObject.parent, REQUIREFIELDS_PARENT );
+            }
+        }
+    } );
+
+    await rankHelper.calculateWobjectRank( wObjects ); // calculate rank for wobject
+
+    return { wObjectsData: wObjects, hasMore };
+};
+
+const getRequiredFields = function ( wObject, requiredFields ) {
+    wObject.fields = wObject.fields.filter( ( item ) => requiredFields.includes( item.name ) );
+};
+
+module.exports = { getMany };
