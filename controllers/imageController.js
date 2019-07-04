f31fc7ab4e7aa0ee76b72ee24d776f9d38fe9a5a
Eugene <eugenezh@123software.ru>
2019-07-04 10:35:31 +0300

    add not resizing params to aws-images

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -8,8 +8,10 @@ const saveImage = async function ( req, res ) {
     if ( error ) {
         res.status( 422 ).send( { error: error } );
     }
-    await image.uploadInS3( base64, fileName, '_small' );
-    await image.uploadInS3( base64, fileName, '_medium' );
+    if( !req.query.notResizing ) {
+        await image.uploadInS3( base64, fileName, '_small' );
+        await image.uploadInS3( base64, fileName, '_medium' );
+    }
     res.status( 200 ).json( { image: imageUrl } );
 };
 
