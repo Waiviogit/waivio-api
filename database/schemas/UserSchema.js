bf4ec53ba53d7caea38f3bdda6ec692d70a1fa66
Maxim Oancha <maksimoancha12@gmail.com>
2019-07-30 13:17:18 +0300

    change user schema, add field alias, add migration to create alias

--- database/schemas/UserSchema.js
+++ database/schemas/UserSchema.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema( {
     name: { type: String, index: true, unique: true },
+    alias: { type: String },
     profile_image: { type: String },
     read_locales: { type: [ String ], default: [] },
     objects_follow: { type: [ String ], default: [] }, // arr of author_permlink of objects what user following
