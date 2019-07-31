8a60733e5108103f213e7ef2f957c2507ebdf32d
Maxim Oancha <maksimoancha12@gmail.com>
2019-07-31 17:51:35 +0300

    add json_metadata to user schema
    add to user schema drafts, app_settings, bookmarks

--- database/schemas/UserSchema.js
+++ database/schemas/UserSchema.js
@@ -1,5 +1,38 @@
 const mongoose = require( 'mongoose' );
 const Schema = mongoose.Schema;
+const { LANGUAGES } = require( '../../utilities/constants' );
+
+const UserMetadataSchema = new Schema( {
+    notifications_last_timestamp: { type: Number, default: null },
+    settings: {
+        exitPageSetting: { type: Boolean, default: false }, // Enable this option to use the exit page when clicking on an external link.
+        locale: { type: String, enum: [ ...LANGUAGES ], default: 'auto' }, // which language use on waivio
+        postLocales: { type: [ { type: String, enum: [ ...LANGUAGES ] } ], default: [] }, // in which language do you want read posts
+        nightmode: { type: Boolean, default: false }, // toggle nightmode on UI
+        rewardSetting: { type: String, enum: [ 'SP', '50', 'STEEM' ], default: '50' }, // in which format get rewards from posts
+        rewriteLinks: { type: Boolean, default: false }, // change links from steemit.com to waivio.com
+        showNSFWPosts: { type: Boolean, default: false }, // show or hide NSFW posts
+        upvoteSetting: { type: Boolean, default: false }, // enable auto like on your posts
+        votePercent: { type: Number, min: 1, max: 10000, default: 5000 }, // default percent of your upvotes
+        votingPower: { type: Boolean, default: false } // dynamic toggle of vote power on each vote
+    },
+    bookmarks: { type: [ String ], default: [] },
+    drafts: {
+        type: [ {
+            title: { type: String },
+            author: { type: String },
+            beneficiary: { type: Boolean, default: true },
+            body: { type: String },
+            jsonMetadata: { type: Object },
+            lastUpdated: { type: Number },
+            parentAuthor: { type: String },
+            parentPermlink: { type: String },
+            permlink: { type: String },
+            reward: { type: String }
+        } ],
+        default: []
+    }
+} );
 
 const UserSchema = new Schema( {
     name: { type: String, index: true, unique: true },
@@ -11,7 +44,7 @@ const UserSchema = new Schema( {
     json_metadata: { type: String, default: '' },
     wobjects_weight: { type: Number, default: 0 }, // sum of weight of all wobjects
     count_posts: { type: Number, default: 0, index: true },
-    app_settings: { type: Object, default: [] } // custom settings like night_mode, default percent of vote etc.
+    user_metadata: { type: UserMetadataSchema, default: () => ( {} ) }
 }, { timestamps: true } );
 
 UserSchema.index( { wobjects_weight: -1 } );
