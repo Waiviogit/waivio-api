ea332bd5909621f74eb7b9e5abe2013952609193
Maxim Oancha <maksimoancha12@gmail.com>
2019-08-13 13:04:36 +0300

    refactor and add cron to update TOP every 30 minutes
    Replace handle of updating top wobjects by ObjectType to operation, create Cron Job to update TOP every 30 minutes

--- migrations/1565619668649-add-to-object-type-top-wobjects.js
+++ migrations/1565619668649-add-to-object-type-top-wobjects.js
@@ -1,25 +1,12 @@
 'use strict';
-const { WObject, ObjectType } = require( '../database' ).models;
-const { OBJECT_TYPE_TOP_WOBJECTS_COUNT } = require( '../utilities/constants' );
+const { ObjectType } = require( '../database' ).models;
+const updateObjectTypes = require( '../utilities/operations/objectType/updateTopWobjects' );
 /**
  * Make any changes you need to make to the database here
  */
 
 exports.up = async function up ( done ) {
-    let cursor = ObjectType.find().cursor( { batchSize: 1000 } );
-
-    await cursor.eachAsync( async ( doc ) => {
-        const wobjs_array = await WObject
-            .find( { 'object_type': doc.name } )
-            .sort( { weight: -1 } )
-            .limit( OBJECT_TYPE_TOP_WOBJECTS_COUNT );
-        const author_permlinks = wobjs_array.map( ( p ) => p.author_permlink );
-        const res = await ObjectType.updateOne( { _id: doc._id }, { $set: { top_wobjects: author_permlinks } } );
-
-        if( res.nModified ) {
-            console.log( `Object Type ${doc.name} updated! Add ${author_permlinks.length} wobjects refs!` );
-        }
-    } );
+    await updateObjectTypes( true );
     done();
 };
 /**
---
+++ utilities/operations/objectType/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+    updateTopWobjects: require( './updateTopWobjects' )
+};
