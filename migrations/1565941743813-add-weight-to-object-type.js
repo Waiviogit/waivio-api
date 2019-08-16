2918a0867f0857c945546cb63ad22e724ab29962
Maxim Oancha <maksimoancha12@gmail.com>
2019-08-16 11:07:53 +0300

    create migration on object type weight
    create migration to calculate weight for all existing wobjects and object types

---
+++ migrations/1565941743813-add-weight-to-object-type.js
@@ -0,0 +1,30 @@
+'use strict';
+const { ObjectType, WObject } = require( '../database' ).models;
+/**
+ * Make any changes you need to make to the database here
+ */
+
+exports.up = async function up ( done ) {
+    let cursor = ObjectType.find().cursor( { batchSize: 1000 } );
+
+    await cursor.eachAsync( async ( doc ) => {
+        const [ { total_weight } ] = await WObject.aggregate( [
+            { $match: { object_type: doc.name } },
+            { $group: { _id: null, total_weight: { $sum: '$weight' } } }
+        ] );
+        const res = await ObjectType.updateOne( { _id: doc._id }, { $set: { weight: total_weight } } );
+
+        if( res.nModified ) {
+            console.log( `ObjectType ${doc.name} updated! Now "Type" has ${total_weight} weight!` );
+        }
+    } );
+    done();
+};
+/**
+ * Make any changes that UNDO the up function side effects here (if possible)
+ */
+exports.down = async function down( done ) {
+    await ObjectType.update( {}, { $unset: { weight: '' } } );
+    console.log( 'Deleted field "weight" from all of ObjectTypes!' );
+    done();
+};
