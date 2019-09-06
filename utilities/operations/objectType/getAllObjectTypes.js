34baa8cb0b93878cefce2024386f13154a0baede
Maxim Oancha <maksimoancha12@gmail.com>
2019-09-06 10:59:06 +0300

    change method of getting related wobjects on list object types

--- utilities/operations/objectType/getAllObjectTypes.js
+++ utilities/operations/objectType/getAllObjectTypes.js
@@ -1,31 +1,31 @@
-const { ObjectType } = require( '../../../models' );
-const _ = require( 'lodash' );
+const { ObjectType, Wobj } = require( '../../../models' );
 
-const makePipeline = ( { limit, skip } ) => {
+const objectTypePipeline = ( { limit, skip, wobjects_count } ) => {
     const pipeline = [
         { $skip: skip },
-        {
-            $lookup: {
-                from: 'wobjects',
-                localField: 'top_wobjects',
-                foreignField: 'author_permlink',
-                as: 'related_wobjects'
-            }
-        }
+        { $addFields: { top_wobjects: { $slice: [ '$top_wobjects', wobjects_count ] } } }
     ];
 
     if( limit ) pipeline.splice( 1, 0, { $limit: limit } );
     return pipeline;
 };
 
-module.exports = async ( { limit, skip, wobjects_count = 3 } ) => {
-    const { result: objectTypes, error } = await ObjectType.aggregate(
-        makePipeline( { limit, skip } )
-    );
+const relatedWobjectsPipeline = ( author_permlinks ) => {
+    return [
+        { $match: { author_permlink: { $in: author_permlinks } } },
+        { $sort: { weight: -1, _id: -1 } }
+    ];
+};
 
+module.exports = async ( { limit, skip, wobjects_count = 3 } ) => {
+    const { result: objectTypes, error } = await ObjectType.aggregate( objectTypePipeline( { limit, skip, wobjects_count: wobjects_count + 1 } ) );
     if( error ) return { error };
+
     for( let type of objectTypes ) {
-        type.related_wobjects = _.orderBy( type.related_wobjects, [ 'weight' ], [ 'desc' ] );
+        const { wobjects = [], error: wobjError } = await Wobj.fromAggregation( relatedWobjectsPipeline( type.top_wobjects ) );
+        if( wobjError ) return { error: wobjError };
+
+        type.related_wobjects = wobjects;
         if( type.related_wobjects.length > wobjects_count ) {
             type.hasMoreWobjects = true;
             type.related_wobjects = type.related_wobjects.slice( 0, wobjects_count );
