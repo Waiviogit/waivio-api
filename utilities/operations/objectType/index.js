17bceea4e1c13c882f635cd3146514ab943301a7
Maxim Oancha <maksimoancha12@gmail.com>
2019-09-19 11:42:55 +0300

    add route for getting object type experts

--- utilities/operations/objectType/index.js
+++ utilities/operations/objectType/index.js
@@ -2,5 +2,6 @@ module.exports = {
     updateTopWobjects: require( './updateTopWobjects' ),
     getAll: require( './getAllObjectTypes' ),
     getOne: require( './getOneObjectType' ),
-    updateTopExperts: require( './updateTopExperts' )
+    updateTopExperts: require( './updateTopExperts' ),
+    getExperts: require( './getExperts' )
 };
