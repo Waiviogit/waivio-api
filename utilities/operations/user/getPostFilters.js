e9860a6f76687d3bdf213cf08c8ba9b3b6d09148
Maksim <maksimoancha12@gmail.com>
2019-11-25 11:46:05 +0200

    fix

--- utilities/operations/user/getPostFilters.js
+++ utilities/operations/user/getPostFilters.js
@@ -5,7 +5,7 @@ module.exports = async ( { name, skip = 0, limit = 30 } ) => {
         { $match: { author: name, reblog_to: null } },
         { $unwind: '$wobjects' },
         { $group: { _id: '$wobjects.author_permlink', posts_count: { $sum: 1 } } },
-        { $sort: { posts_count: -1 } },
+        { $sort: { posts_count: -1, _id: -1 } },
         { $skip: skip },
         { $limit: limit },
         { $project: { _id: 0, author_permlink: '$_id', posts_count: 1 } },
