bab49c9aaac8f4638a5f121e7ee1554bdca89cc4
Maksim <maksimoancha12@gmail.com>
2019-11-27 11:36:11 +0200

    fix

--- controllers/validators/userValidator.js
+++ controllers/validators/userValidator.js
@@ -40,8 +40,11 @@ exports.objectsFeedSchema = Joi.object().keys( {
 exports.feedSchema = Joi.object().keys( {
     name: Joi.string().required(),
     limit: Joi.number().integer().min( 0 ).max( 50 ).default( 20 ),
-    start_author: Joi.string().allow( '' ).default( '' ),
-    start_permlink: Joi.string().allow( '' ).default( '' )
+    skip: Joi.number().integer().min( 0 ).default( 0 ),
+    filter: Joi.object().keys( {
+        byApp: Joi.string().allow( '' )
+    } ),
+    user_languages: Joi.array().items( Joi.string().valid( [ ...LANGUAGES ] ) ).default( [ 'ru-RU' ] )
 } );
 
 exports.searchSchema = Joi.object().keys( {
@@ -100,8 +103,8 @@ exports.updateMetadataSchema = Joi.object().keys( {
 exports.blogSchema = Joi.object().keys( {
     name: Joi.string().required(),
     limit: Joi.number().integer().min( 0 ).max( 100 ).default( 20 ),
-    skip: Joi.number().integer().min( 0 ).default( 0 ),
-    author_permlinks: Joi.array().items( Joi.string().required() ).default( null )
+    start_author: Joi.string().allow( '' ).default( '' ),
+    start_permlink: Joi.string().allow( '' ).default( '' )
 } );
 
 exports.followingUpdates = Joi.object().keys( {
