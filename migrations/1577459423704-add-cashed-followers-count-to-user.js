483ce96bd7e17932876e815683f55515779c9d75
Maksim <maksimoancha12@gmail.com>
2019-12-28 13:13:09 +0200

    fix migration

--- migrations/1577459423704-add-cashed-followers-count-to-user.js
+++ migrations/1577459423704-add-cashed-followers-count-to-user.js
@@ -10,17 +10,14 @@ const { User } = require( '../database' ).models;
 exports.up = async function up ( done ) {
     let cursor = User.find().cursor( { batchSize: 1000 } );
     await cursor.eachAsync( async ( doc ) => {
-        const user = await User.findOne( { name: doc.name } ).populate( 'followers_count_virtual' ).lean();
-        if( user && user.followers_count_virtual ) {
-            const res = await User.updateOne( { name: doc.name }, { $set: { followers_count: user.followers_count_virtual } } );
-            if ( res.nModified ) {
-                console.log( `User ${doc.name} "followers_count" updated!` );
-            }
+        const count = await User.find( { users_follow: doc.name } ).count();
+        const res = await User.updateOne( { name: doc.name }, { $set: { followers_count: count || 0 } } );
+        if ( res.nModified ) {
+            console.log( `User ${doc.name} "followers_count" updated!` );
         }
     } );
     done();
 };
-
 
 /**
  * Make any changes that UNDO the up function side effects here (if possible)
