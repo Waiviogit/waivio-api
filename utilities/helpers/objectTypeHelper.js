9fe6bd57ba2a63eb2cc99d4f01df677db8a5609f
olegtsigulov <olegcigulev@gmail.com>
2020-02-07 11:17:04 +0200

    add check for array

--- utilities/helpers/objectTypeHelper.js
+++ utilities/helpers/objectTypeHelper.js
@@ -2,24 +2,52 @@ const moment = require('moment');
 const _ = require('lodash');
 const { User, Wobj, paymentHistory } = require('models');
 
+const campaignValidation = (campaign) => !!(campaign.reservation_timetable[moment().format('dddd').toLowerCase()]
+    && _.floor(campaign.budget / campaign.reward) > _.filter(campaign.users, (user) => user.status === 'assigned'
+        && user.createdAt > moment().startOf('month')).length);
 
-exports.campaignFilter = async (campaigns) => {
+const requirementFilters = async (campaign, user) => {
+  let frequency = [], notBlacklisted = true;
+  if (user && user.name) {
+    notBlacklisted = !_.includes(campaign.blacklist_users, user.name);
+    frequency = _
+      .chain(campaign.users)
+      .filter((doer) => doer.name === user.name && doer.status === 'completed')
+      .sortBy('updatedAt')
+      .compact()
+      .value();
+  }
+  return {
+    can_assign_by_current_day: true,
+    can_assign_by_budget: campaign.budget > campaign.reward * _.filter(campaign.users, (doer) => doer.status === 'assigned').length,
+    posts: user ? user.count_posts >= campaign.userRequirements.minPosts : false,
+    followers: user ? user.count_posts >= campaign.userRequirements.minPosts : false,
+    expertise: user ? user.wobjects_weight >= campaign.userRequirements.minExpertise : false,
+    freeReservation: true,
+    frequency: frequency.length ? moment().startOf('month') > frequency[frequency.length - 1].updatedAt : true,
+    not_blacklisted: notBlacklisted,
+  };
+};
+
+exports.campaignFilter = async (campaigns, user) => {
   const validCampaigns = [];
   await Promise.all(campaigns.map(async (campaign) => {
     if (campaignValidation(campaign)) {
       const { result, error } = await Wobj.findOne(campaign.requiredObject);
       if (error) return;
       campaign.required_object = result;
-      const { user, error: userError } = await User.getOne(campaign.guideName);
-      if (userError || !user) return;
+      const { user: guide, error: guideError } = await User.getOne(campaign.guideName);
+      if (guideError || !guide) return;
 
       const { result: totalPayed } = await paymentHistory.findByCondition(
         { sponsor: campaign.guideName, type: 'transfer' },
       );
+      campaign.assigned = user ? !!_.find(campaign.users, (doer) => doer.name === user.name && doer.status === 'assigned') : false;
+      campaign.requirement_filters = await requirementFilters(campaign, user);
       campaign.guide = {
         name: campaign.guideName,
-        wobjects_weight: user.wobjects_weight,
-        alias: user.alias,
+        wobjects_weight: guide.wobjects_weight,
+        alias: guide.alias,
         totalPayed: _.sumBy(totalPayed, (count) => count.amount),
       };
       validCampaigns.push(campaign);
@@ -27,7 +55,3 @@ exports.campaignFilter = async (campaigns) => {
   }));
   return validCampaigns;
 };
-
-const campaignValidation = (campaign) => !!(campaign.reservation_timetable[moment().format('dddd').toLowerCase()]
-      && _.floor(campaign.budget / campaign.reward) > _.filter(campaign.users, (user) => user.status === 'assigned'
-          && user.createdAt > moment().startOf('month')).length);
