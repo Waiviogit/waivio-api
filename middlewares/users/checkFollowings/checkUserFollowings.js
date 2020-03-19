5d62cd59816abe26a7262a62995893c77e2cfea6
olegtsigulov <olegcigulev@gmail.com>
2020-03-19 10:43:31 +0200

    start

--- middlewares/users/checkFollowings/checkUserFollowings.js
+++ middlewares/users/checkFollowings/checkUserFollowings.js
@@ -2,6 +2,18 @@ const _ = require('lodash');
 const { User } = require('models');
 const { schema } = require('middlewares/users/checkFollowers/schema');
 
-exports.check = async () => {
+exports.check = async (req, res, next) => {
+  const currentSchema = schema.find((s) => s.path === req.route.path && s.method === req.method);
 
+  if (!currentSchema || !req.headers.follower) {
+    return next();
+  }
+
+  switch (currentSchema.case) {
+    case 1:
+      break;
+    case 2:
+      break;
+  }
+  next();
 };
