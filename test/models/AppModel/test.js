7fb7fee27196a87655ebc671d048d582e8ea5a6f
sergeypronyuk <sergeypronyuk@gmail.com>
2020-03-26 17:50:15 +0200

    Add AppModule test

---
+++ test/models/AppModel/test.js
@@ -0,0 +1,16 @@
+const { expect, AppModel, faker } = require('test/testHelper');
+const { AppFactory } = require('test/models/factory/AppFactory/AppFactory');
+
+describe('App Model', async () => {
+  describe('On getOne', async () => {
+    let app,
+      result;
+    beforeEach(async () => {
+      app = await AppFactory.CreatedaData();
+    });
+    it('Should check names for identity', async () => {
+      result = await AppModel.getOne({ name: app.name });
+      expect(result.app).to.deep.eq(app);
+    });
+  });
+});
---
+++ test/models/factory/AppFactory/AppFactory.js
@@ -0,0 +1,29 @@
+const { faker, App } = require('test/testHelper');
+
+const CreatedaData = async ({
+  blacklists, name, admins, moderators, topUsers,
+} = {}) => {
+  const data = {
+    name: name || faker.random.string(10),
+    admins: admins || [faker.name.firstName().toLowerCase()],
+    moderators: moderators || [],
+    topUsers: topUsers || [],
+    blacklists: blacklists || {
+      users: [], wobjects: [], posts: [], apps: [],
+    },
+    daily_chosen_post: {
+      author: faker.name.firstName().toLowerCase(),
+      permlink: faker.random.string(),
+      title: faker.random.string(20),
+    },
+    weekly_chosen_post: {
+      author: faker.name.firstName().toLowerCase(),
+      permlink: faker.random.string(),
+      title: faker.random.string(20),
+    },
+  };
+
+  return App.create(data);
+};
+
+module.exports = { CreatedaData };
