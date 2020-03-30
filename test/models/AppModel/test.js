01fea75aa78120279e98dab0027677b63b29e6e1
Sergey <sergeypronyuk@gmail.com>
2020-03-30 23:40:03 +0300

    Completed tasks given in pull request.

--- test/models/AppModel/test.js
+++ test/models/AppModel/test.js
@@ -10,55 +10,73 @@ describe('App Model', async () => {
     beforeEach(async () => {
       app = await AppFactory.Create();
     });
-    it('Should check names for identity', async () => {
-      result = await AppModel.getOne({ name: app.name });
-      expect(result.app).to.deep.eq(app._doc);
+    it('Should check app for identity', async () => {
+      result = await AppModel.getOne({ name: app.name, bots: 0 });
+      expect(result.app).to.deep.eq(_.omit(app._doc, 'service_bots'));
     });
     it('Should check that the error exists', async () => {
       result = await AppModel.getOne({ name: faker.random.string() });
-      expect(result.error).is.exist;
+      expect(result.error).to.be.exist;
     });
     it('Should return error message', async () => {
       result = await AppModel.getOne({ name: faker.random.string() });
-      expect(result.error.message).to.eq('App not found!');
+      expect(result.error.message).to.be.eq('App not found!');
     });
   });
   describe('On getAll', async () => {
-    let iteration, result;
+    let result, appsCount;
     beforeEach(async () => {
       await dropDatabase();
-      for (iteration = 0; iteration < 5; iteration++) {
-        await AppFactory.Create();
-      }
     });
-    it('Should check number of entries', async () => {
-      result = await AppModel.getAll();
-      expect(result.apps.length).to.eq(iteration);
+    describe('On success case', async () => {
+      beforeEach(async () => {
+        appsCount = 5;
+        for (let iteration = 0; iteration < appsCount; iteration++) {
+          await AppFactory.Create();
+        }
+      });
+      it('Should check that getAll returns correct number of records', async () => {
+        result = await AppModel.getAll();
+        expect(result.apps.length).to.be.eq(appsCount);
+      });
     });
-    it('Should check that the error exists', async () => {
-      await dropDatabase();
-      result = await AppModel.getAll();
-      expect(result.error).is.exist;
-    });
-    it('Should return error message', async () => {
-      await dropDatabase();
-      result = await AppModel.getAll();
-      expect(result.error.message).to.eq('App not found!');
+    describe('On error case', async () => {
+      beforeEach(async () => {
+        result = await AppModel.getAll();
+      });
+      it('Should check that the error exists', async () => {
+        expect(result.error).is.exist;
+      });
+      it('Should return error message', async () => {
+        expect(result.error.message).to.be.eq('App not found!');
+      });
     });
   });
   describe('On aggregate', async () => {
-    let name, admins, blacklists, supportedHashtags, supportedObjects, app, aggResult, result;
+    let name, admins, blacklists, app, aggResult, result;
     beforeEach(async () => {
+      await dropDatabase();
       name = faker.name.firstName();
       admins = [faker.name.firstName().toLowerCase()];
       blacklists = [faker.name.firstName().toLowerCase()];
-      supportedHashtags = [faker.random.string(5)];
-      supportedObjects = [faker.random.string(5)];
       app = await AppFactory.Create({
-        name, admins, blacklists, supportedHashtags, supportedObjects,
+        name, admins, blacklists,
       });
     });
-    it('Should return from our request: name and admins', async () => {
+    it('Should return from our request: id, name and admins. Using group stage', async () => {
+      aggResult = await AppModel.aggregate([
+        {
+          $group: {
+            _id: '$_id',
+            name: { $first: '$name' },
+            admins: { $first: '$admins' },
+          },
+        },
+      ]);
+      [result] = aggResult.result;
+      expect(result).to.deep.eq(_.pick(app, ['_id', 'name', 'admins']));
+    });
+    it('Should return from our request: name and admins. Using match and project stage', async () => {
       aggResult = await AppModel.aggregate([
         { $match: { name: app.name } },
         {
@@ -70,41 +88,12 @@ describe('App Model', async () => {
       [result] = aggResult.result;
       expect(result).to.deep.eq(_.pick(app, ['name', 'admins']));
     });
-    it('Should return from our request: name and blacklist users', async () => {
+    it('Should return from our request: whole app. Using match stage', async () => {
       aggResult = await AppModel.aggregate([
         { $match: { name: app.name } },
-        {
-          $project: {
-            _id: 0, name: 1, blacklists: 1,
-          },
-        },
       ]);
       [result] = aggResult.result;
-      expect(result).to.deep.eq(_.pick(app, ['name', 'blacklists']));
-    });
-    it('Should return from our request: name, blacklist users, supported Hashtags, supported Objects', async () => {
-      aggResult = await AppModel.aggregate([
-        { $match: { name: app.name } },
-        {
-          $project: {
-            _id: 0, name: 1, blacklists: 1, supported_hashtags: 1, supported_objects: 1,
-          },
-        },
-      ]);
-      [result] = aggResult.result;
-      expect(result).to.deep.eq(_.pick(app, ['name', 'blacklists', 'supported_hashtags', 'supported_objects']));
-    });
-    it('There is no blacklist as a result of our request', async () => {
-      aggResult = await AppModel.aggregate([
-        { $match: { name: app.name } },
-        {
-          $project: {
-            _id: 0, name: 1, admins: 1,
-          },
-        },
-      ]);
-      [result] = aggResult.result;
-      expect(result).to.not.have.property('blacklists');
+      expect(result).to.deep.eq(app._doc);
     });
     it('Should return undefined, when aggregate has not result', async () => {
       aggResult = await AppModel.aggregate([
@@ -120,12 +109,12 @@ describe('App Model', async () => {
     });
   });
   describe('On updateOne', async () => {
-    let name, app1, app, result;
+    let name, app, result;
     beforeEach(async () => {
       name = faker.name.firstName();
-      app1 = await AppFactory.Create({ name });
+      app = await AppFactory.Create({ name });
     });
-    it('Update name at our request and get true', async () => {
+    it('Should update name at request and get true', async () => {
       result = await AppModel.updateOne({ name, updData: { $set: { name: faker.name.firstName() } } });
       expect(result.result).to.be.true;
     });
@@ -134,9 +123,9 @@ describe('App Model', async () => {
       expect(result.result).to.be.false;
     });
     it('Should return duplicate key error, when the data has duplicate name', async () => {
-      app = await AppFactory.Create({ name: faker.name.firstName() });
-      result = await AppModel.updateOne({ name: app1.name, updData: { $set: { name: app.name } } });
-      expect(result.error).is.exist;
+      const appDuplicate = await AppFactory.Create({ name: faker.name.firstName() });
+      result = await AppModel.updateOne({ name: app.name, updData: { $set: { name: appDuplicate.name } } });
+      expect(result.error).to.be.exist;
     });
   });
 });
