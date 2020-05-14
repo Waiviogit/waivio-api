51d73b1b7ec7bc8a0256d53ac9a57db56754026c
olegtsigulov <olegcigulev@gmail.com>
2020-05-14 13:37:11 +0300

    add filtered cache feed

--- utilities/redis/redisGetter.js
+++ utilities/redis/redisGetter.js
@@ -1,6 +1,6 @@
 const { wobjRefsClient, importUserClient, mainFeedsCacheClient } = require('utilities/redis/redis');
 const {
-  HOT_NEWS_CACHE_PREFIX, TREND_NEWS_CACHE_PREFIX, HOT_NEWS_CACHE_SIZE, TREND_NEWS_CACHE_SIZE,
+  HOT_NEWS_CACHE_PREFIX, HOT_NEWS_CACHE_SIZE, TREND_NEWS_CACHE_SIZE, TREND_NEWS_CACHE_PREFIX,
 } = require('utilities/constants');
 
 /**
@@ -47,13 +47,16 @@ exports.getHotFeedCache = async ({ limit = 10, locale, app }) => {
  * @param limit {Number} count of ids to return
  * @param locale {String} specified locale from list of allowed languages
  * @param app {String} List ids by locales with moderation for specified App
+ * @param prefix
  * @returns {Promise<{error: string}|{ids: *}>}
  */
-exports.getTrendFeedCache = async ({ limit = 10, locale, app }) => {
+exports.getTrendFeedCache = async ({
+  limit = 10, locale, app, prefix = TREND_NEWS_CACHE_PREFIX,
+}) => {
   if (!locale) locale = 'en-US';
   if (limit > TREND_NEWS_CACHE_SIZE) return { error: `skip param should be less than ${TREND_NEWS_CACHE_SIZE}` };
   const appPrefix = app ? `${app}:` : '';
   return {
-    ids: await mainFeedsCacheClient.lrangeAsync(`${appPrefix}${TREND_NEWS_CACHE_PREFIX}:${locale}`, 0, limit - 1),
+    ids: await mainFeedsCacheClient.lrangeAsync(`${appPrefix}${prefix}:${locale}`, 0, limit - 1),
   };
 };
