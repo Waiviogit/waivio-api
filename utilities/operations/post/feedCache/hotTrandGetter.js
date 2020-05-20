95521ae2b8d06b7659b3511c2ee34295e2658969
olegtsigulov <olegcigulev@gmail.com>
2020-05-20 15:16:21 +0300

    add beaxy cache

--- utilities/operations/post/feedCache/hotTrandGetter.js
+++ utilities/operations/post/feedCache/hotTrandGetter.js
@@ -47,7 +47,7 @@ exports.getHot = async ({
  * @returns {Promise<{posts}|{error: *}|undefined>}
  */
 exports.getTrend = async ({
-   skip, limit, locales, forApp, prefix,
+  skip, limit, locales, forApp, prefix,
 }) => {
 // get array of arrays of ids, for every locale
   const idLocaleArrays = await Promise.all(locales.map(async (locale) => {
@@ -74,35 +74,38 @@ exports.getTrend = async ({
   });
 };
 
-/**
- * Comparator for feed cache indexes,
- * use it for array.sort() by Descending order
- * @param a
- * @param b
- * @returns {number} For e.x. "X". X = 0 if equal, X<0 if a larger, X>0 if b larger
- */
-function idComparator(a, b) {
-  // in one of input items miss => return another item
-  if (!a || !b) return a || b;
-  const aNum = parseInt(a.split('_')[0], 10);
-  const bNum = parseInt(b.split('_')[0], 10);
-  return bNum - aNum;
-}
-
-function findLargestIndex(items, comparator) {
-  let largestIndex = 0;
-  for (const itemIdx in items) {
-    if (comparator(items[itemIdx], items[largestIndex]) < 0) {
-      largestIndex = itemIdx;
-    }
-  }
-  return largestIndex;
-}
-
-/*
-Get post Mongo _id from redis cache item
- */
-const obtainPostId = (item) => item.split('_')[1];
+// /**
+//  * Comparator for feed cache indexes,
+//  * use it for array.sort() by Descending order
+//  * @param a
+//  * @param b
+//  * @returns {number} For e.x. "X". X = 0 if equal, X<0 if a larger, X>0 if b larger
+//  */
+// function idComparator(a, b) {
+//   // in one of input items miss => return another item
+//   if (!a && !b) return 0;
+//   if (!a) return -parseInt(b.split('_')[0], 10);
+//   if (!b) return -parseInt(a.split('_')[0], 10);
+//
+//   const aNum = parseInt(a.split('_')[0], 10);
+//   const bNum = parseInt(b.split('_')[0], 10);
+//   return bNum - aNum;
+// }
+//
+// function findLargestIndex(items, comparator) {
+//   let largestIndex = 0;
+//   for (const itemIdx in items) {
+//     if (comparator(items[itemIdx], items[largestIndex]) < 0) {
+//       largestIndex = itemIdx;
+//     }
+//   }
+//   return largestIndex;
+// }
+//
+// /*
+// Get post Mongo _id from redis cache item
+//  */
+// const obtainPostId = (item) => item.split('_')[1];
 
 /*
 Get top N items from several arrays of ids.
@@ -111,30 +114,17 @@ and get top N => use this custom method for only
 get top N items by compare each array top element.
  */
 function getTopFromArrays(arrays, topCount) {
-  const indexes = _.times(arrays.length, () => 0);
-  const result = [];
-  while (result.length < topCount) {
-    const topValues = indexes.map((value, idx) => _.get(arrays, `[${idx}][${value}]`));
-
-    /* if every item in "topValues" is undefined/null
-    it means that all arrays now is empty.
-    Break the loop and return results */
-    if (_.every(topValues, _.isNil)) break;
-
-    // get index of array with current largest value
-    const largestIndex = findLargestIndex(topValues, idComparator);
-    /*
-    arrays[largestIndex] => return index of feed by locale with largest item currently handling
-    indexes[largestIndex] => return current handling index of item in specified feed locale
-     */
-    const topValue = _.get(arrays, `[${largestIndex}][${indexes[largestIndex]}]`);
-    if (topValue) {
-      result.push(obtainPostId(topValue));
-    }
-
-    indexes[largestIndex] += 1;
-  }
-  return result;
+  return _
+    .chain(arrays)
+    .flattenDeep()
+    .map((post) => {
+      const data = post.split('_');
+      return { id: data[1], weight: data[0] };
+    })
+    .orderBy(['weight'], ['desc'])
+    .slice(0, topCount)
+    .map('id')
+    .value();
 }
 
 const getFromDb = async ({ cond, sort }) => {
