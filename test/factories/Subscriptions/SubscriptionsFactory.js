cb6e3509a5217f0405cbf058b51c023ce0ea7975
Boris Andrzhievskyi <borees2@gmail.com>
2020-06-18 16:58:46 +0300

    add tests

---
+++ test/factories/Subscriptions/SubscriptionsFactory.js
@@ -0,0 +1,18 @@
+const { Subscriptions, faker } = require('../../testHelper');
+
+const Create = async ({
+  follower, following, onlyData,
+} = {}) => {
+  const subscriptionData = {
+    follower: follower || faker.name.firstName(),
+    following: following || faker.name.firstName(),
+  };
+  if (onlyData) return subscriptionData;
+  const subscription = new Subscriptions(subscriptionData);
+  await subscription.save();
+  subscription.toObject();
+
+  return subscription;
+};
+
+module.exports = { Create };
--- test/factories/index.js
+++ test/factories/index.js
@@ -3,4 +3,5 @@ module.exports = {
   CommentFactory: require('./CommentFactory/CommentFactory'),
   PostFactory: require('./PostFactory/PostFactory'),
   ObjectFactory: require('./ObjectFactory/ObjectFactory'),
+  SubscriptionsFactory: require('./Subscriptions/SubscriptionsFactory'),
 };
