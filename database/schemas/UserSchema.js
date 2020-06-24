fed71c4bc8e8165ac29a0f312792f87f51da15cc
Boris Andrzhievskyi <borees2@gmail.com>
2020-06-24 12:47:13 +0300

    2290 add notification field to UserSchema and write migration

--- database/schemas/UserSchema.js
+++ database/schemas/UserSchema.js
@@ -4,6 +4,24 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const { LANGUAGES } = require('../../utilities/constants');
 
+const UserNotificationsSchema = new Schema({
+  account_witness_vote: { type: Boolean, default: true },
+  activateCampaign: { type: Boolean, default: true },
+  changePassword: { type: Boolean, default: true },
+  change_recovery_account: { type: Boolean, default: true },
+  comment: { type: Boolean, default: true },
+  custom_json: { type: Boolean, default: true },
+  fillOrder: { type: Boolean, default: true },
+  rejectUpdate: { type: Boolean, default: true },
+  restaurantStatus: { type: Boolean, default: true },
+  suspendedStatus: { type: Boolean, default: true },
+  transfer: { type: Boolean, default: true },
+  transfer_from_savings: { type: Boolean, default: true },
+  transfer_to_vesting: { type: Boolean, default: true },
+  withdraw_route: { type: Boolean, default: true },
+  withdraw_vesting: { type: Boolean, default: true },
+}, { _id: false });
+
 const UserMetadataSchema = new Schema({
   notifications_last_timestamp: { type: Number, default: 0 },
   settings: {
@@ -22,6 +40,7 @@ const UserMetadataSchema = new Schema({
       type: Number, min: 1, max: 10000, default: 10000,
     }, // default percent of your upvotes
     votingPower: { type: Boolean, default: true }, // dynamic toggle of vote power on each vote
+    userNotifications: { type: UserNotificationsSchema },
   },
   bookmarks: { type: [String], default: [] },
   drafts: {
