ad1975058ba021374d67256e346efc361fd0cdae
Maxim <maksimoancha12@gmail.com>
2020-07-29 11:41:36 +0300

    fix

--- utilities/operations/user/updateMetadata.js
+++ utilities/operations/user/updateMetadata.js
@@ -3,7 +3,7 @@ const { User } = require('models');
 
 // eslint-disable-next-line camelcase
 module.exports = async ({ user_name, user_metadata }) => {
-  user_metadata.drafts = _.slice(user_metadata.drafts, 0, 20);
+  user_metadata.drafts = _.slice(_.orderBy(user_metadata.drafts, ['lastUpdated'], ['desc']), 0, 20);
   const { user, error } = await User.updateOne({ name: user_name }, { $set: { user_metadata } });
 
   if (error) return { error };
