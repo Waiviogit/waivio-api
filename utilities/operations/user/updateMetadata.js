5a617ebe37414bb00a5e687491712608cae6a2c9
Maxim <maksimoancha12@gmail.com>
2020-07-29 11:22:29 +0300

    slice drafts

--- utilities/operations/user/updateMetadata.js
+++ utilities/operations/user/updateMetadata.js
@@ -1,8 +1,9 @@
+const _ = require('lodash');
 const { User } = require('models');
 
 // eslint-disable-next-line camelcase
 module.exports = async ({ user_name, user_metadata }) => {
-  user_metadata.drafts = user_metadata.drafts.slice(21, -1);
+  user_metadata.drafts = _.slice(user_metadata.drafts, 0, 20);
   const { user, error } = await User.updateOne({ name: user_name }, { $set: { user_metadata } });
 
   if (error) return { error };
