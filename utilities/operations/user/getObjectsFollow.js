b7925631f0d246dbbaed51ab1157eed00a7a809c
Maxim <maksimoancha12@gmail.com>
2020-08-06 12:49:45 +0300

    add route for following objects

--- utilities/operations/user/getObjectsFollow.js
+++ utilities/operations/user/getObjectsFollow.js
@@ -1,15 +1,12 @@
 const _ = require('lodash');
-const { User, Wobj } = require('models');
+const { User } = require('models');
 
 module.exports = async (value) => {
   const { wobjects, error } = await User.getObjectsFollow(value);
   if (error) return { error };
   if (!wobjects.length) return { wobjects };
-  const { result: parents } = await Wobj.find({ author_permlink: { $in: _.compact(_.map(wobjects, 'parent')) } });
-
-  return {
-    wobjects: _.forEach(wobjects, (wobj) => {
-      if (wobj.parent) wobj.parent = _.find(parents, { author_permlink: wobj.parent });
-    }),
-  };
+  wobjects.forEach((wObject) => {
+    wObject.fields = _.filter(wObject.fields, (field) => _.includes(['name', 'avatar'], field.name));
+  });
+  return { wobjects };
 };
