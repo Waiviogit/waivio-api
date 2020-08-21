8e19616530c67de895ce01929924ad754087863c
olegtsigulov <olegcigulev@gmail.com>
2020-08-21 21:56:17 +0300

    optimize post wobject

--- database/schemas/UserWobjectsSchema.js
+++ database/schemas/UserWobjectsSchema.js
@@ -9,8 +9,8 @@ const UserWobjectsSchema = new Schema({
 }, { timestamps: false });
 
 UserWobjectsSchema.index({ user_name: 1 });
-// UserWobjectsSchema.index({ author_permlink: 1 });
-//UserWobjectsSchema.index({ author_permlink: 1, user_name: 1 }, { unique: true });
+UserWobjectsSchema.index({ author_permlink: 1 });
+UserWobjectsSchema.index({ author_permlink: 1, user_name: 1 }, { unique: true });
 
 const UserWobjects = mongoose.model('user_wobjects', UserWobjectsSchema);
 
