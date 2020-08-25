09ad329faeff7c09b6453894326d8853b76473a9
Maxim <maksimoancha12@gmail.com>
2020-08-25 11:03:51 +0300

    add check for new user

--- utilities/operations/user/getUserMetadata.js
+++ utilities/operations/user/getUserMetadata.js
@@ -3,8 +3,10 @@ const { User } = require('models');
 module.exports = async (userName) => {
   // method updateOne find user and try to update it, but if user not exist
   // - create and return created new user
-  const { user, error } = await User.updateOne({ name: userName });
+  const { user, error } = await User.getOne(userName, '+user_metadata');
+  if (error || !user) return { error: error || { status: 404, message: 'User not Found!' } };
 
-  if (error) return { error };
+  user.user_metadata.new_user = user.user_metadata.new_user
+      && !user.users_following_count && !user.objects_follow.length;
   return { user_metadata: user.user_metadata, privateEmail: user.privateEmail };
 };
