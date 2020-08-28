aca903ccd652668990673d2c789c940ab4f192b3
Maxim <maksimoancha12@gmail.com>
2020-08-28 14:43:26 +0300

    change child wobj

--- utilities/operations/wobject/index.js
+++ utilities/operations/wobject/index.js
@@ -4,7 +4,6 @@ moduleExports.objectExperts = require('./objectExperts');
 moduleExports.wobjectInfo = require('./wobjectInfo');
 moduleExports.getManyObjects = require('./getManyObjects');
 moduleExports.getPostsByWobject = require('./getPostsByWobject');
-moduleExports.getChildWobjects = require('./getChildWobjects');
 moduleExports.getGallery = require('./getGallery');
 moduleExports.getWobjField = require('./getWobjField');
 
