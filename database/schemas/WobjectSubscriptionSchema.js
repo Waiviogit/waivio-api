6ff674b8cbefb59dacc4d6474b6976750d58812b
Boris Andrzhiievskyi <borees2@gmail.com>
2020-08-31 15:17:37 +0300

    start write migration, works only for first 100 users

---
+++ database/schemas/WobjectSubscriptionSchema.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const WobjectSubscriptionSchema = new Schema({
+  follower: { type: String, required: true },
+  following: { type: String, required: true },
+}, { versionKey: false });
+
+WobjectSubscriptionSchema.index({ follower: 1, following: 1 }, { unique: true });
+WobjectSubscriptionSchema.index({ following: 1 });
+
+WobjectSubscriptionSchema.virtual('followerPath', {
+  ref: 'User',
+  localField: 'follower',
+  foreignField: 'name',
+  justOne: true,
+});
+
+WobjectSubscriptionSchema.virtual('followingPath', {
+  ref: 'User',
+  localField: 'following',
+  foreignField: 'name',
+  justOne: true,
+});
+
+const WobjectSubscriptionModel = mongoose.model('WobjectSubscriptions', WobjectSubscriptionSchema);
+
+module.exports = WobjectSubscriptionModel;
