df16cd8d594786935ea62abe2dd839e66838cce5
Boris Andrzhiievskyi <borees2@gmail.com>
2020-09-04 17:50:45 +0300

    add migration

---
+++ migrations/1598862132594-wobject-subscriptions-collection.js
@@ -0,0 +1,29 @@
+const { WobjectSubscriptions, User } = require('database').models;
+
+/**
+ * Make any changes you need to make to the database here
+ */
+exports.up = async function up(done) {
+  const cursor = User
+    .find().select({ name: 1, objects_follow: 1 }).lean().cursor({ batchSize: 100 });
+  await cursor.eachAsync(async (doc) => {
+    if (!doc.objects_follow.length) return;
+
+    for (const following of doc.objects_follow) {
+      try {
+        const newSubscription = new WobjectSubscriptions({ follower: doc.name, following });
+        await newSubscription.save();
+      } catch (e) {
+        console.error(e.message);
+      }
+    }
+  });
+  done();
+};
+
+/**
+ * Make any changes that UNDO the up function side effects here (if possible)
+ */
+exports.down = async function down(done) {
+  done();
+};
