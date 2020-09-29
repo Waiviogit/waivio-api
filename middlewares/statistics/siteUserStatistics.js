0ee65f59a1707ef1a978c1efbf67a29f002aca3f
olegtsigulov <olegcigulev@gmail.com>
2020-09-29 17:38:41 +0300

    add sites routes

--- middlewares/statistics/siteUserStatistics.js
+++ middlewares/statistics/siteUserStatistics.js
@@ -4,7 +4,7 @@ const { redisSetter } = require('utilities/redis');
 exports.saveUserIp = async (req, res, next) => {
   const { host } = req.headers;
   const ip = req.headers['x-real-ip'];
-  if (!ip) return;
+  if (!ip) return next();
   await redisSetter.addSiteActiveUser(`${redisStatisticsKey}:${host}`, ip);
   next();
 };
