75fd9e0a9e8aba0974432c162a7abbff91285581
Boris Andrzhiievskyi <borees2@gmail.com>
2020-10-07 15:32:19 +0300

    fix pr

--- utilities/tasks/changeAvatarLinks/run.js
+++ utilities/tasks/changeAvatarLinks/run.js
@@ -1,12 +1,6 @@
 const change = require('./changeLinks');
 
 (async () => {
-  const creds = {
-    AWS_ENDPOINT: process.argv[2],
-    AWS_ACCESS_KEY_ID: process.argv[3],
-    AWS_SECRET_ACCESS_KEY: process.argv[4],
-  };
-
-  await change(creds);
+  await change();
   process.exit();
 })();
