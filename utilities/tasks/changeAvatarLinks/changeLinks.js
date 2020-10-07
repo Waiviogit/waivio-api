16a19ceff13349d5896349b21c531d9997011468
Boris Andrzhiievskyi <borees2@gmail.com>
2020-10-07 15:28:53 +0300

    fix pr

--- utilities/tasks/changeAvatarLinks/changeLinks.js
+++ utilities/tasks/changeAvatarLinks/changeLinks.js
@@ -1,25 +1,21 @@
 const _ = require('lodash');
-const AWS = require('aws-sdk');
-const sharp = require('sharp');
+const axios = require('axios');
+const FormData = require('form-data');
 const { WObject } = require('database').models;
-const { base64ByUrl, generateFileName } = require('utilities/helpers/imagesHelper');
 
-module.exports = async ({ AWS_ENDPOINT, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY }) => {
+module.exports = async () => {
   const wobjects = await WObject.aggregate([
     { $match: { $or: [{ 'fields.name': 'avatar' }, { 'fields.name': 'background' }, { 'fields.name': 'galleryItem' }] } },
     { $unwind: '$fields' },
     { $match: { 'fields.body': { $regex: /^http:/ } } },
     { $project: { fields: 1, author_permlink: 1 } },
   ]);
-  const image = new Image({ AWS_ENDPOINT, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY });
   const fields = _.map(wobjects, (el) => ({
     ...el.fields,
     author_permlink: el.author_permlink,
   }));
   for (const field of fields) {
-    const base64 = await base64ByUrl(field.body);
-    const fileName = await generateFileName({});
-    const { imageUrl } = await image.uploadInS3(base64, fileName);
+    const imageUrl = await uploadImage(field.body);
     await WObject.updateOne(
       { author_permlink: field.author_permlink, 'fields._id': field._id },
       { $set: { 'fields.$.body': imageUrl } },
@@ -27,57 +23,20 @@ module.exports = async ({ AWS_ENDPOINT, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
   }
 };
 
-class Image {
-  constructor({ AWS_ENDPOINT, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY }) {
-    const spacesEndpoint = new AWS.Endpoint(AWS_ENDPOINT);
-
-    this._s3 = new AWS.S3({
-      endpoint: spacesEndpoint,
-      accessKeyId: AWS_ACCESS_KEY_ID,
-      secretAccessKey: AWS_SECRET_ACCESS_KEY,
-      params: {
-        Bucket: 'waivio',
-        ACL: 'public-read',
-        ContentType: 'image/webp',
-        ContentEncoding: 'base64',
+const uploadImage = async (url) => {
+  try {
+    const bodyFormData = new FormData();
+    bodyFormData.append('imageUrl', url);
+    const result = await axios({
+      method: 'post',
+      url: 'https://waiviodev.com/api/image',
+      data: bodyFormData,
+      headers: {
+        'content-type': `multipart/form-data; boundary=${bodyFormData._boundary}`,
       },
     });
+    return result.data.image;
+  } catch (error) {
+    console.error(error.message);
   }
-
-  async uploadInS3(base64, fileName, size = '') {
-    if (base64) {
-      try {
-        // eslint-disable-next-line no-buffer-constructor
-        const buffer = new Buffer(base64, 'base64');
-        const body = await this.resizeImage({ buffer, size });
-
-        return new Promise((resolve) => {
-          this._s3.upload({ Body: body, Key: `${fileName}${size}` }, (err, data) => {
-            if (err) {
-              resolve({ error: `Error upload image:${err}` });
-            } if (data) {
-              resolve({ imageUrl: data.Location });
-            }
-          });
-        });
-      } catch (error) {
-        return { error };
-      }
-    }
-    return { error: 'Error parse image' };
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  async resizeImage({ buffer, size }) {
-    if (size === '_small') {
-      return sharp(buffer).rotate(0).resize(34, 34).toBuffer();
-    } if (size === '_medium') {
-      return sharp(buffer).rotate(0).resize(180, 180).toBuffer();
-    }
-    if (buffer.byteLength > 1500000) {
-      return sharp(buffer).resize(1980).withMetadata().toFormat('jpeg')
-        .toBuffer();
-    }
-    return buffer;
-  }
-}
+};
