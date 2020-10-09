9f1e97935c8a4f738f730bf9f9c18f16ae29ddd1
olegtsigulov <olegcigulev@gmail.com>
2020-10-09 18:26:36 +0300

    search

--- utilities/operations/search/searchWobjects.js
+++ utilities/operations/search/searchWobjects.js
@@ -1,5 +1,5 @@
 const _ = require('lodash');
-const { FIELDS_NAMES } = require('constants/wobjectsData');
+const { FIELDS_NAMES, SEARCH_FIELDS } = require('constants/wobjectsData');
 const { Wobj, App } = require('models');
 const { getNamespace } = require('cls-hooked');
 
@@ -8,12 +8,8 @@ exports.searchWobjects = async ({
   string, object_type, limit, skip, crucialWobjects, forParent, required_fields,
 }) => {
   // get count of wobjects grouped by object_types
-  // const {
-  //   wobjects: wobjectsCounts,
-  //   error: getWobjCountError,
-  // } = await Wobj.fromAggregation(makeCountPipeline({ string }));
-  // let crucialWobjects = [];
   let app;
+
   if (!crucialWobjects) {
     const session = getNamespace('request-session');
     const host = session.get('host');
@@ -22,21 +18,37 @@ exports.searchWobjects = async ({
 
     crucialWobjects = _.get(app, 'supported_objects', []);
   }
-  // get wobjects
-  const { wObject } = await Wobj.getOne(string, object_type, true);
 
-  const pipeline = crucialWobjects
-    ? addFieldsToSearch({
-      crucialWobjects, authorities: _.get(app, 'authority'), string, limit, skip, forParent, object_type,
-    })
-    : makePipeline({
-      string, object_type, limit, skip, crucialWobjects, forParent, required_fields,
-    });
+  const forSites = crucialWobjects.length && (app.inherited || app.canBeExtended);
+  const authorities = _.get(app, 'authority', []);
+  const supportedTypes = _.get(app, 'supported_object_types', []);
+
+  const {
+    wobjects: wobjectsCounts,
+    error: getWobjCountError,
+  } = await Wobj.fromAggregation(makeCountPipeline({
+    string, crucialWobjects, authorities, object_type, forSites, supportedTypes,
+  }));
+
+  const pipeline = getPipeline({
+    forSites,
+    crucialWobjects,
+    authorities,
+    string,
+    limit,
+    skip,
+    forParent,
+    object_type,
+    supportedTypes,
+    required_fields,
+  });
 
   const { wobjects = [], error: getWobjError } = await Wobj.fromAggregation(pipeline);
-  if (wObject && wobjects.length) {
-    _.remove(wobjects, (wobj) => wObject.author_permlink === wobj.author_permlink);
-    wobjects.splice(0, 0, wObject);
+  const wobject = _.find(wobjects, { author_permlink: string });
+
+  if (wobject && wobjects.length) {
+    _.remove(wobjects, (wobj) => wobject.author_permlink === wobj.author_permlink);
+    wobjects.splice(0, 0, wobject);
   }
   return {
     wobjects: _.take(wobjects, limit),
@@ -45,49 +57,35 @@ exports.searchWobjects = async ({
   };
 };
 
-const addFieldsToSearch = ({
-  crucialWobjects, string, authorities, object_type, forParent, skip, limit,
-}) => [
-  // { fields: { $elemMatch: { name: FIELDS_NAMES.ADDRESS, body: { $regex: string, $options: 'i' } } } },
-  // { fields: { $elemMatch: { name: FIELDS_NAMES.TITLE, body: { $regex: string, $options: 'i' } } } },
-  // { fields: { $elemMatch: { name: FIELDS_NAMES.DESCRIPTION, body: { $regex: string, $options: 'i' } } } },
-  // { fields: { $elemMatch: { name: FIELDS_NAMES.CATEGORY_ITEM, body: { $regex: string, $options: 'i' } } } },
-  {
-    $project: {
-      fields: 1,
-      ownership: { $setIntersection: [authorities, '$authority.ownership'] },
-      administrative: { $setIntersection: [authorities, '$authority.administrative'] },
-      author_permlink: 1,
-      object_type: 1,
-    },
-  },
-  {
-    $match: {
-      $and: [{
-        $or: [
-          { author_permlink: { $regex: `${_.get(string, '[3]') === '-' ? `^${string}` : '_'}`, $options: 'i' } },
-          { ownership: { $ne: { $size: 0 } } },
-          { administrative: { $ne: { $size: 0 } } },
-          { fields: { $elemMatch: { name: { $in: [FIELDS_NAMES.NAME, FIELDS_NAMES.ADDRESS] }, body: { $regex: string, $options: 'i' } } } },
-        ],
-      },
+const getPipeline = ({
+  forSites, crucialWobjects, authorities, string, limit,
+  skip, forParent, object_type, supportedTypes, required_fields,
+}) => {
+  const pipeline = forSites
+    ? addFieldsToSearch({
+      crucialWobjects, authorities, string, limit, skip, forParent, object_type, supportedTypes,
+    })
+    : makePipeline({
+      string, object_type, limit, skip, crucialWobjects, forParent, required_fields,
+    });
+  return pipeline;
+};
 
-      // eslint-disable-next-line camelcase
-      { object_type: { $regex: `^${object_type || '.*'}$`, $options: 'i' } },
-      ],
-      'status.title': { $nin: ['unavailable', 'nsfw', 'relisted'] },
-    },
-  },
+const addFieldsToSearch = ({
+  crucialWobjects, string, authorities, object_type, forParent, skip, limit, supportedTypes,
+}) => [
+  ...matchSitesPipe({
+    string, authorities, crucialWobjects, object_type, supportedTypes,
+  }),
   {
     $addFields: {
-      // eslint-disable-next-line camelcase
-      crucial_wobject: { $cond: { if: { $in: ['$author_permlink', crucialWobjects] }, then: 1, else: 0 } },
       priority: { $cond: { if: { $eq: ['$parent', forParent] }, then: 1, else: 0 } },
     },
   },
-  { $sort: { crucial_wobject: -1, priority: -1, weight: -1 } },
+  { $sort: { priority: -1, weight: -1 } },
   { $limit: limit || 10 },
-  { $skip: skip || 0 }];
+  { $skip: skip || 0 },
+];
 
 const makePipeline = ({
   // eslint-disable-next-line camelcase
@@ -119,16 +117,69 @@ const makePipeline = ({
   { $skip: skip || 0 },
 ];
 
-const makeCountPipeline = ({ string }) => [
+const makeCountPipeline = ({
+  string, forSites, authorities, crucialWobjects, object_type, supportedTypes,
+}) => {
+  const pipeline = [
+    { $group: { _id: '$object_type', count: { $sum: 1 } } },
+    { $project: { _id: 0, object_type: '$_id', count: 1 } },
+  ];
+  if (forSites) {
+    pipeline.unshift(...matchSitesPipe({
+      string, authorities, crucialWobjects, object_type, supportedTypes,
+    }));
+  } else {
+    pipeline.unshift({
+      $match: {
+        $or: [
+          { fields: { $elemMatch: { name: 'name', body: { $regex: string, $options: 'i' } } } },
+          { author_permlink: { $regex: `${_.get(string, '[3]') === '-' ? `^${string}` : '_'}`, $options: 'i' } },
+        ],
+        'status.title': { $nin: ['unavailable', 'nsfw', 'relisted'] },
+      },
+    });
+  }
+  return pipeline;
+};
+
+const matchSitesPipe = ({
+  authorities, crucialWobjects, string, object_type, supportedTypes,
+}) => [
   {
     $match: {
-      $or: [
-        { fields: { $elemMatch: { name: 'name', body: { $regex: string, $options: 'i' } } } },
-        { author_permlink: { $regex: `${_.get(string, '[3]') === '-' ? `^${string}` : '_'}`, $options: 'i' } },
+      $or: [{
+        $expr: {
+          $gt: [
+            { $size: { $setIntersection: ['$authority.ownership', authorities] } },
+            0,
+          ],
+        },
+      }, {
+        $expr: {
+          $gt: [
+            { $size: { $setIntersection: ['$authority.administrative', authorities] } },
+            0,
+          ],
+        },
+      },
+      { author_permlink: { $in: crucialWobjects } },
       ],
+      object_type: { $in: supportedTypes },
       'status.title': { $nin: ['unavailable', 'nsfw', 'relisted'] },
     },
   },
-  { $group: { _id: '$object_type', count: { $sum: 1 } } },
-  { $project: { _id: 0, object_type: '$_id', count: 1 } },
+  {
+    $match: {
+      $and: [
+        {
+          $or: [
+            { author_permlink: { $regex: `${_.get(string, '[3]') === '-' ? `^${string}` : '_'}`, $options: 'i' } },
+            { fields: { $elemMatch: { name: { $in: SEARCH_FIELDS }, body: { $regex: string, $options: 'i' } } } },
+          ],
+        },
+        // eslint-disable-next-line camelcase
+        { object_type: { $regex: `^${object_type || '.*'}$`, $options: 'i' } },
+      ],
+    },
+  },
 ];
