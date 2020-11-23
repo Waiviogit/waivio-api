503629cb347492f2ecf0905e258a1afe5f0ac17e
Boris Andrzhiievskyi <borees2@gmail.com>
2020-11-23 16:22:34 +0200

    change schema

--- database/schemas/RelatedAlbumSchema.js
+++ database/schemas/RelatedAlbumSchema.js
@@ -3,11 +3,12 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const RelatedAlbumSchema = new Schema({
-  id: { type: String, required: true },
-  body: { type: String, required: true },
+  wobjAuthorPermlink: { type: String, required: true },
+  postAuthorPermlink: { type: String, required: true },
+  images: { type: [String], default: [] },
 }, { versionKey: false });
 
-RelatedAlbumSchema.index({ id: 1, body: 1 }, { unique: true });
+RelatedAlbumSchema.index({ wobjAuthorPermlink: 1, postAuthorPermlink: 1 }, { unique: true });
 
 const RelatedAlbumModel = mongoose.model('related_album', RelatedAlbumSchema);
 
