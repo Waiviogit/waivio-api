32274837b8dcc8784ec8e8878e546638bd4e2f52
Boris Andrzhiievskyi <borees2@gmail.com>
2021-01-04 18:52:31 +0200

    3344-refactor function balanceNotification

--- utilities/operations/sites/sitesNotifications.js
+++ utilities/operations/sites/sitesNotifications.js
@@ -1,5 +1,4 @@
 const _ = require('lodash');
-const moment = require('moment');
 const { App } = require('models');
 const { redisSetter } = require('utilities/redis');
 const { NOTIFICATION } = require('constants/sitesConstants');
@@ -10,10 +9,6 @@ exports.balanceNotification = async () => {
   const { result } = await App.find({ inherited: true, canBeExtended: false });
   const owners = _.uniq(_.map(result, 'owner'));
   if (_.isEmpty(owners)) return;
-  const threeMonth = Math.ceil(moment.duration(moment().add(3, 'month').startOf('day').diff(moment().startOf('day'))).asDays());
-  const twoMonth = Math.ceil(moment.duration(moment().add(2, 'month').startOf('day').diff(moment().startOf('day'))).asDays());
-  const oneMonth = Math.ceil(moment.duration(moment().add(1, 'month').startOf('day').diff(moment().startOf('day'))).asDays());
-  const week = 7;
   const requestData = [];
 
   for (const owner of owners) {
@@ -21,39 +16,35 @@ exports.balanceNotification = async () => {
     if (error) continue;
     const remainingDays = _.get(accountBalance, 'remainingDays', 0);
     const paid = _.get(accountBalance, 'paid');
-    if (paid < 0) {
-      const suspendedDays = await redisSetter
-        .incrementWebsitesSuspended({ key: owner, expire: 3600 * 25 });
-      if (suspendedDays < 8) {
-        requestData.push({ owner, message: NOTIFICATION.ATTENTION });
-        continue;
-      }
-    }
-    if (_.includes(_.range(1, 6), remainingDays)) {
-      requestData.push({ owner, message: `${NOTIFICATION.WARNING} ${remainingDays} ${remainingDays === 1 ? 'day' : 'days'}` });
-      continue;
-    }
-    switch (remainingDays) {
-      case threeMonth:
-        requestData.push({ owner, message: `${NOTIFICATION.WARNING} 3 month` });
-        continue;
-      case twoMonth:
-        requestData.push({ owner, message: `${NOTIFICATION.WARNING} 2 month` });
-        continue;
-      case oneMonth:
-        requestData.push({ owner, message: `${NOTIFICATION.WARNING} 1 month` });
-        continue;
-      case week * 3:
-        requestData.push({ owner, message: `${NOTIFICATION.WARNING} 3 weeks` });
-        continue;
-      case week * 2:
-        requestData.push({ owner, message: `${NOTIFICATION.WARNING} 2 weeks` });
-        continue;
-      case week:
-        requestData.push({ owner, message: `${NOTIFICATION.WARNING} a week` });
-        continue;
-    }
+    const message = await getMessage({ remainingDays, paid, owner });
+    if (message) requestData.push({ owner, message });
   }
+
   if (_.isEmpty(requestData)) return;
   return notificationsHelper.sendNotification({ id: NOTIFICATION.BALANCE_ID, data: requestData });
+};
+
+const getMessage = async ({ remainingDays, paid, owner }) => {
+  if (paid < 0) {
+    const suspendedDays = await redisSetter
+      .incrementWebsitesSuspended({ key: owner, expire: 3600 * 25 });
+    if (suspendedDays < 8) return NOTIFICATION.ATTENTION;
+    return '';
+  }
+  const messages = {
+    90: () => `${NOTIFICATION.WARNING} 3 month`,
+    60: () => `${NOTIFICATION.WARNING} 2 month`,
+    30: () => `${NOTIFICATION.WARNING} 1 month`,
+    21: () => `${NOTIFICATION.WARNING} 3 weeks`,
+    14: () => `${NOTIFICATION.WARNING} 2 weeks`,
+    7: () => `${NOTIFICATION.WARNING} a week`,
+    6: () => `${NOTIFICATION.WARNING} 6 days`,
+    5: () => `${NOTIFICATION.WARNING} 5 days`,
+    4: () => `${NOTIFICATION.WARNING} 4 days`,
+    3: () => `${NOTIFICATION.WARNING} 3 days`,
+    2: () => `${NOTIFICATION.WARNING} 2 days`,
+    1: () => `${NOTIFICATION.WARNING} a day`,
+    default: () => '',
+  };
+  return (messages[remainingDays] || messages.default)();
 };
