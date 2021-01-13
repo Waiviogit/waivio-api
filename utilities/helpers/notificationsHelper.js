2184e99c4e668a33d31d1fbbfe745db152ae3846
Boris Andrzhiievskyi <borees2@gmail.com>
2021-01-13 11:00:23 +0200

    3344 - send sentry notification

--- utilities/helpers/notificationsHelper.js
+++ utilities/helpers/notificationsHelper.js
@@ -2,6 +2,8 @@ const axios = require('axios');
 const {
   HOST, BASE_URL, SET_NOTIFICATION,
 } = require('constants/requestData').NOTIFICATIONS_API;
+const Sentry = require('@sentry/node');
+const { sendSentryNotification } = require('utilities/helpers/sentryHelper');
 
 exports.sendNotification = async (reqData) => {
   const URL = HOST + BASE_URL + SET_NOTIFICATION;
@@ -10,6 +12,7 @@ exports.sendNotification = async (reqData) => {
   try {
     await axios.post(URL, reqData, { headers: { API_KEY } });
   } catch (error) {
-    console.log(error.message);
+    Sentry.captureException(error);
+    return sendSentryNotification(error);
   }
 };
