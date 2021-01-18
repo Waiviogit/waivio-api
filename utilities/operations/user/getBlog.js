4c8ceee96ac85d61b5174529a37f42e8c6d95c8f
Boris Andrzhiievskyi <borees2@gmail.com>
2021-01-18 15:18:11 +0200

    3632 fix task getTagsByUser

--- utilities/operations/user/getBlog.js
+++ utilities/operations/user/getBlog.js
@@ -5,7 +5,6 @@ const {
 const { getTagsByUser } = require('utilities/helpers/postHelper');
 
 module.exports = async ({
-  // eslint-disable-next-line camelcase
   name, limit, skip, userName, app, tagsArray,
 }) => {
   const { result: mutedUsers } = await mutedUserModel.find({
@@ -13,29 +12,23 @@ module.exports = async ({
   });
   if (!_.isEmpty(mutedUsers)) return { posts: [], tags: [] };
 
-  const { user, error: userError } = await User.getOne(name);
+  const { error: userError } = await User.getOne(name);
   const { hiddenPosts = [] } = await hiddenPostModel.getHiddenPosts(userName);
   const additionalCond = {};
   if (!_.isEmpty(hiddenPosts)) Object.assign(additionalCond, { _id: { $nin: hiddenPosts } });
   if (!_.isEmpty(tagsArray)) Object.assign(additionalCond, { 'wobjects.author_permlink': { $in: tagsArray } });
 
-  if (userError) return { error: userError };
-  if (user && user.auth) {
-    return Post.getBlog({
-      name, limit, skip, additionalCond,
-    });
-  }
-  const { posts, error } = await Post.getBlog({
-    limit, name, skip: skip !== 0 ? skip - 1 : 0, additionalCond,
+  const { posts, error: postError } = await Post.getBlog({
+    limit, name, skip, additionalCond,
   });
 
-  if (error) return { error };
+  if (userError || postError) return { error: userError || postError };
 
   // add field reblogged_by if post not authored by "user" blog requested
   posts.forEach((post) => {
     if (post.author !== name) post.reblogged_by = [name];
   });
-
   const { tags } = await getTagsByUser({ author: name, skip });
+
   return { tags, posts };
 };
