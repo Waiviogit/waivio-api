28b5293a9ebbbbddf4eabcc006b3f82bc845f4c5
Boris Andrzhiievskyi <borees2@gmail.com>
2021-02-09 15:36:11 +0200

    Git-1982 - move method to helpers

--- utilities/helpers/sitesHelper.js
+++ utilities/helpers/sitesHelper.js
@@ -225,3 +225,30 @@ exports.getSettings = async (host) => {
 
   return { result: _.pick(app, ['googleAnalyticsTag', 'beneficiary']) };
 };
+
+exports.distanceInMBetweenEarthCoordinates = ([lon1, lat1], [lon2, lat2]) => {
+  const earthRadiusM = 6371000;
+  const center = [mediumPoint(lon1, lon2), mediumPoint(lat1, lat2)];
+
+  const dLat = degreesToRadians(lat2 - lat1);
+  const dLon = degreesToRadians(lon2 - lon1);
+
+  const lat1Rad = degreesToRadians(lat1);
+  const lat2Rad = degreesToRadians(lat2);
+
+  const a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+    + Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1Rad) * Math.cos(lat2Rad);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  const distance = earthRadiusM * c;
+
+  return {
+    min: Math.round(distance / 40),
+    max: Math.round(distance / 2),
+    center,
+  };
+};
+
+const degreesToRadians = (degrees) => (degrees * Math.PI) / 180;
+
+const mediumPoint = (point1, point2) => (point1 + point2) / 2;
--- utilities/operations/sites/map.js
+++ utilities/operations/sites/map.js
@@ -1,6 +1,6 @@
 const _ = require('lodash');
 const { Wobj, User } = require('models');
-const { campaignsHelper } = require('utilities/helpers');
+const { campaignsHelper, sitesHelper } = require('utilities/helpers');
 
 exports.getData = async ({
   topPoint, bottomPoint, limit, userName, skip, app,
@@ -10,7 +10,8 @@ exports.getData = async ({
   const supportedTypes = _.get(app, 'supported_object_types', []);
   const forSites = _.get(app, 'inherited');
 
-  const { min, max, center } = distanceInmBetweenEarthCoordinates(topPoint, bottomPoint);
+  const { min, max, center } = sitesHelper
+    .distanceInMBetweenEarthCoordinates(topPoint, bottomPoint);
 
   const condition = {
     map: {
@@ -41,30 +42,3 @@ exports.getData = async ({
     },
   };
 };
-
-const distanceInmBetweenEarthCoordinates = ([lon1, lat1], [lon2, lat2]) => {
-  const earthRadiusM = 6371000;
-  const center = [mediumPoint(lon1, lon2), mediumPoint(lat1, lat2)];
-
-  const dLat = degreesToRadians(lat2 - lat1);
-  const dLon = degreesToRadians(lon2 - lon1);
-
-  const lat1Rad = degreesToRadians(lat1);
-  const lat2Rad = degreesToRadians(lat2);
-
-  const a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
-    + Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1Rad) * Math.cos(lat2Rad);
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-  const distance = earthRadiusM * c;
-
-  return {
-    min: Math.round(distance / 40),
-    max: Math.round(distance / 2),
-    center,
-  };
-};
-
-const degreesToRadians = (degrees) => (degrees * Math.PI) / 180;
-
-const mediumPoint = (point1, point2) => (point1 + point2) / 2;
