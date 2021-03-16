0dc2d195168768c25fae8861f476ef3d3d1f6386
Boris Andrzhiievskyi <borees2@gmail.com>
2021-03-16 17:05:29 +0200

    Git-2235 fix manage page

--- utilities/operations/sites/manage.js
+++ utilities/operations/sites/manage.js
@@ -1,6 +1,7 @@
-const _ = require('lodash');
 const { PAYMENT_TYPES, FEE, STATUSES } = require('constants/sitesConstants');
 const { sitesHelper } = require('utilities/helpers');
+const BigNumber = require('bignumber.js');
+const _ = require('lodash');
 
 /** Get data for manage page. In this method, we generate a report for the site owner,
  * in which we include the average data on users on his sites for the last 7 days,
@@ -12,19 +13,17 @@ exports.getManagePage = async ({ userName }) => {
   const accountBalance = {
     paid: 0, avgDau: 0, dailyCost: 0, remainingDays: 0,
   };
-  accountBalance.paid = _.sumBy(payments, (payment) => {
-    if (payment.type === PAYMENT_TYPES.TRANSFER) return payment.amount;
-  }) || 0;
+
+  accountBalance.paid = getSumByPaymentType(payments, PAYMENT_TYPES.TRANSFER)
+    .minus(getSumByPaymentType(payments, PAYMENT_TYPES.WRITE_OFF))
+    .toNumber();
+
   const dataForPayments = await sitesHelper.getPaymentsData();
   const prices = {
     minimumValue: FEE.minimumValue,
     perSuspended: FEE.perInactive,
     perUser: FEE.perUser,
   };
-
-  accountBalance.paid -= _.sumBy(payments, (payment) => {
-    if (payment.type !== PAYMENT_TYPES.TRANSFER) return payment.amount;
-  }) || 0;
 
   if (!apps.length) {
     return {
@@ -56,3 +55,9 @@ exports.getManagePage = async ({ userName }) => {
     prices,
   };
 };
+
+const getSumByPaymentType = (payments, type) => _
+  .chain(payments)
+  .filter((el) => el.type === type)
+  .reduce((acc, payment) => BigNumber(payment.amount).plus(acc), BigNumber(0))
+  .value();
