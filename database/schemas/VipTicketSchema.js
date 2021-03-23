f9b55435666ec021bb79490800f11f0ae0a18a43
Boris Andrzhiievskyi <borees2@gmail.com>
2021-03-23 15:28:48 +0200

    Git-2146 -add swagger.json

--- database/schemas/VipTicketSchema.js
+++ database/schemas/VipTicketSchema.js
@@ -4,7 +4,9 @@ const { Schema } = mongoose;
 
 const VipTicketSchema = new Schema({
   userName: { type: String, required: true, index: true },
-  ticket: { type: String, required: true, unique: true },
+  ticket: {
+    type: String, required: true, unique: true, index: true,
+  },
   valid: { type: Boolean, default: true },
   note: { type: String },
 }, { versionKey: false, timestamps: true });
