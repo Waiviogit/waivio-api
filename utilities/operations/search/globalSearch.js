d57ea674986af43f0e7d70b00f1e2bd65edd9969
Yaroslav Semeniuk <sem.yarik1@gmail.com>
2021-04-30 10:01:45 +0300

    fixed parameters for search

--- utilities/operations/search/globalSearch.js
+++ utilities/operations/search/globalSearch.js
@@ -8,15 +8,15 @@ exports.getGlobalSearch = async ({
   searchString, userLimit, wobjectsLimit, objectsTypeLimit, sortByApp,
 }) => {
   const { result: app } = await getSessionApp();
-  const escapedReservedCharacters = searchString.replace(/[.?+*|{}[\]()"\\@]/g, '\\\\$&');
+  const escapedString = searchString.replace(/[.?+*|{}[\]()"\\@]/g, '\\\\$&');
 
   const { objectTypes, objectTypesCount } = await searchObjectTypes(
-    { string: escapedReservedCharacters, limit: objectsTypeLimit, supportedTypes: _.get(app, 'supported_object_types', []) },
+    { string: searchString, limit: objectsTypeLimit, supportedTypes: _.get(app, 'supported_object_types', []) },
   );
   const { wobjects, wobjectsCounts } = await searchWobjects({
-    string: escapedReservedCharacters, limit: wobjectsLimit, sortByApp, needCounters: true, app,
+    string: escapedString, limit: wobjectsLimit, sortByApp, needCounters: true, app,
   });
-  const { users, usersCount } = await searchUsers({ string: escapedReservedCharacters, limit: userLimit });
+  const { users, usersCount } = await searchUsers({ string: searchString, limit: userLimit });
 
   return {
     objectTypes, objectTypesCount, wobjects, wobjectsCounts, users, usersCount,
