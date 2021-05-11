fab3a134faa49d6dc06f9637249726705eb49835
Boris Andrzhiievskyi <borees2@gmail.com>
2021-05-11 14:56:22 +0300

    Git-2546-fix bug

--- utilities/operations/user/getUserMetadata.js
+++ utilities/operations/user/getUserMetadata.js
@@ -3,7 +3,7 @@ const { User, wobjectSubscriptions } = require('models');
 module.exports = async (userName) => {
   // method updateOne find user and try to update it, but if user not exist
   // - create and return created new user
-  const { user, error } = await User.getOne(userName, '+user_metadata');
+  const { user, error } = await User.getOne(userName, ['+user_metadata', '+privateEmail']);
   if (error || !user) return { error: error || { status: 404, message: 'User not Found!' } };
   const { count: objectsFollowCount } = await wobjectSubscriptions
     .getFollowingsCount(user.name);
