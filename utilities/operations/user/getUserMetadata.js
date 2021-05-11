120aa497f992be4cc356187f558c0f5e72b7b78d
Boris Andrzhiievskyi <borees2@gmail.com>
2021-05-11 15:09:16 +0300

    Git-2546-remove irrelevant comments

--- utilities/operations/user/getUserMetadata.js
+++ utilities/operations/user/getUserMetadata.js
@@ -1,8 +1,6 @@
 const { User, wobjectSubscriptions } = require('models');
 
 module.exports = async (userName) => {
-  // method updateOne find user and try to update it, but if user not exist
-  // - create and return created new user
   const { user, error } = await User.getOne(userName, ['+user_metadata', '+privateEmail']);
   if (error || !user) return { error: error || { status: 404, message: 'User not Found!' } };
   const { count: objectsFollowCount } = await wobjectSubscriptions
