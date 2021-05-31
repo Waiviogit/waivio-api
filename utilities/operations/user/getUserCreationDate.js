8e44c0f17f3e4f07d094a116f14071e8640b4d35
Boris Andrzhiievskyi <borees2@gmail.com>
2021-05-31 12:27:31 +0300

    Git-2655-timestamp endpoint

---
+++ utilities/operations/user/getUserCreationDate.js
@@ -0,0 +1,25 @@
+const { userUtil } = require('utilities/steemApi');
+const { GUEST_NAME } = require('constants/regExp');
+const { User } = require('models');
+const moment = require('moment');
+
+module.exports = async (name) => {
+  if (!name) return { error: { status: 404, message: 'Not Found' } };
+  return GUEST_NAME.test(name)
+    ? getGuestCreationDate(name)
+    : getHiveCreationDate(name);
+};
+
+const getGuestCreationDate = async (name) => {
+  const { user, error } = await User.getOne(name);
+  if (error) return { error };
+
+  return { timestamp: moment(user.createdAt).valueOf() };
+};
+
+const getHiveCreationDate = async (name) => {
+  const { userData = {}, error } = await userUtil.getAccount(name);
+  if (error) return { error };
+
+  return { timestamp: moment(userData.created).valueOf() };
+};
