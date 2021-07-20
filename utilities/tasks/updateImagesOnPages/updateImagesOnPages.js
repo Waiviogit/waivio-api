7d5bd9ab3c8a7da18ae6894159e85642a09e6611
Yaroslav Semeniuk <sem.yarik1@gmail.com>
2021-07-20 18:24:26 +0300

    created updater links inside fields.body on pages fields

--- utilities/tasks/updateImagesOnPages/updateImagesOnPages.js
+++ utilities/tasks/updateImagesOnPages/updateImagesOnPages.js
@@ -7,22 +7,37 @@ const { base64ByUrl } = require('utilities/helpers/imagesHelper');
 exports.updateImagesOnPages = async () => {
   const wobjects = await WObject.find({ object_type: 'page' }).lean();
   for (const wobject of wobjects) {
-    const pageContents = _.filter(wobject.fields,
-      (field) => field.name === FIELDS_NAMES.PAGE_CONTENT);
+    await updatePageContent(wobject);
+  }
+};
 
-    for (const pageContent of pageContents) {
-      await updateImageLinks(pageContent);
-    }
+const updatePageContent = async (wobject) => {
+  const pageContents = _.filter(wobject.fields,
+    (field) => field.name === FIELDS_NAMES.PAGE_CONTENT);
+  for (const pageContent of pageContents) {
+    await updateImageLinks(pageContent);
   }
 };
 
 const updateImageLinks = async (pageContent) => {
-  const link = pageContent.body.match(/https:\/\/ipfs.busy.org\/ipfs\/[\w]+/);
+  const text = _.get(pageContent, 'body', '');
+  const link = text.match(/https:\/\/ipfs.busy.org\/ipfs\/[\w]+/);
   if (_.isNull(link)) return;
   const base64 = await base64ByUrl(`https://images.hive.blog/0x0/${link}`);
-  if (_.isNull(base64)) return;
-  const { imageUrl, error } = await image.uploadInS3(base64, link.match(/([^/]+$)/));
-  if (error) console.log(`Error download ${link.match(/([^/]+$)/)} to S3`);
-  await WObject.updateOne({ 'fields.permlink': pageContent.permlink }, [{ $addFields: { 'fields.body': imageUrl } }]);
-  await updateImageLinks(pageContent);
+  if (_.isNull(base64)) {
+    const updatedWobj = await WObject.findOneAndUpdate(
+      { 'fields.permlink': pageContent.permlink },
+      { 'fields.$.body': text.replace(link, 'https://waivio.nyc3.digitaloceanspaces.com/ImageNotFound') },
+      { new: true },
+    ).lean();
+    await updatePageContent(updatedWobj);
+  }
+  const { imageUrl, error } = await image.uploadInS3(base64, link[0].match(/[^/]+$/));
+  if (error) console.log(`Error download ${link[0].match(/[^/]+$/)} to S3`);
+  const updatedWobj = await WObject.findOneAndUpdate(
+    { 'fields.permlink': pageContent.permlink },
+    { 'fields.$.body': text.replace(link, imageUrl) },
+    { new: true },
+  ).lean();
+  await updatePageContent(updatedWobj);
 };
