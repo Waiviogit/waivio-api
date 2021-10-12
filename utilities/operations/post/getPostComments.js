3643ec408d77d9cae79c78fbb30b7db15d92e33c
Boris Andrzhiievskyi <borees2@gmail.com>
2021-10-12 10:20:05 +0300

    parentComment

--- utilities/operations/post/getPostComments.js
+++ utilities/operations/post/getPostComments.js
@@ -68,7 +68,7 @@ const removeRepliesFromComment = ({ comments, comment }) => {
     comments,
     (root) => root.author === comment.parent_author && root.permlink === comment.parent_permlink,
   );
-  if (!parentComment && !parentComment.replies) return;
+  if (!parentComment) return;
   parentComment.replies = _.filter(
     parentComment.replies,
     (permlink) => permlink !== `${comment.author}/${comment.permlink}`,
