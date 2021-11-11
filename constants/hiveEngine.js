50ac4f5ea9ee964e5f815367f111ab621e6257cd
Boris Andrzhiievskyi <borees2@gmail.com>
2021-11-11 10:24:36 +0200

    Git-3194 - add waiv tags

--- constants/hiveEngine.js
+++ constants/hiveEngine.js
@@ -2,7 +2,7 @@ exports.TOKEN_WAIV = {
   SYMBOL: 'WAIV',
   POOL_ID: 13,
   DIESEL_POOL_ID: 63,
-  TAGS: ['waivio', 'neoxian', 'palnet'],
+  TAGS: ['waivio', 'neoxian', 'palnet', 'waiv', 'food'],
 };
 
 exports.REDIS_ENGINE_CURATORS = 'engineCurators';
