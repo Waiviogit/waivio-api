84f644b44a21e27d25cb38dadc879029aa2e66f4
Boris Andrzhiievskyi <borees2@gmail.com>
2021-11-23 14:34:50 +0200

    Git_3193 add route for estimate vote panel

--- utilities/operations/user/calcVoteValue.js
+++ utilities/operations/user/calcVoteValue.js
@@ -2,7 +2,7 @@ const engineOperations = require('utilities/hiveEngine/engineOperations');
 const hiveOperations = require('utilities/hiveApi/hiveOperations');
 const { TOKEN_WAIV } = require('constants/hiveEngine');
 
-module.exports = async ({
+exports.sliderCalc = async ({
   userName, weight, author, permlink,
 }) => {
   const requests = await Promise.all([
@@ -20,4 +20,25 @@ module.exports = async ({
   const [hive, waiv] = requests;
 
   return hive.hiveVotePrice + waiv.engineVotePrice;
+};
+
+exports.userInfoCalc = async ({ userName }) => {
+  const requests = await Promise.all([
+    hiveOperations.calcHiveVoteValue({
+      userName,
+    }),
+    engineOperations.calculateHiveEngineVote({
+      symbol: TOKEN_WAIV.SYMBOL,
+      account: userName,
+      poolId: TOKEN_WAIV.POOL_ID,
+      dieselPoolId: TOKEN_WAIV.DIESEL_POOL_ID,
+      weight: 10000,
+    }),
+  ]);
+  const [hive, waiv] = requests;
+
+  return {
+    estimatedHIVE: hive.estimatedHIVE,
+    estimatedWAIV: waiv.engineVotePrice,
+  };
 };
