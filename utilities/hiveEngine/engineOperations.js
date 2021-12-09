c1c2c162d2f72c648e75b2940a1042e7b4f45f75
Boris Andrzhiievskyi <borees2@gmail.com>
2021-12-09 15:14:26 +0200

    fix_quick_like_guest

--- utilities/hiveEngine/engineOperations.js
+++ utilities/hiveEngine/engineOperations.js
@@ -1,30 +1,32 @@
 const _ = require('lodash');
 const { redisGetter } = require('utilities/redis');
+const { CACHE_KEY } = require('constants/common');
 const commentContract = require('./commentContract');
-const tokensContract = require('./tokensContract');
 const marketPools = require('./marketPools');
 
 exports.calculateHiveEngineVote = async ({
   symbol, account, poolId, weight, dieselPoolId,
 }) => {
+  const { rewardPool, pendingClaims } = await redisGetter
+    .importUserClientHGetAll(`${CACHE_KEY.SMT_POOL}:${symbol}`);
+  const rewards = parseFloat(rewardPool) / parseFloat(pendingClaims);
+
   const requests = await Promise.all([
-    tokensContract.getTokenBalances({ query: { symbol, account } }),
     commentContract.getVotingPower({ query: { rewardPoolId: poolId, account } }),
     marketPools.getMarketPools({ query: { _id: dieselPoolId } }),
     commentContract.getRewardPools({ query: { _id: poolId } }),
-    redisGetter.importUserClientHGetAll('current_price_info'),
+    redisGetter.importUserClientHGetAll(CACHE_KEY.CURRENT_PRICE_INFO),
   ]);
 
   for (const req of requests) {
     if (_.has(req, 'error') || _.isEmpty(req)) {
-      return { engineVotePrice: 0, rshares: 0, rewards: 0 };
+      return { engineVotePrice: 0, rshares: 0, rewards };
     }
   }
-  const [balances, votingPowers, dieselPools, smtPool, hiveCurrency] = requests;
+  const [balances, votingPowers, dieselPools, hiveCurrency] = requests;
   const { stake, delegationsIn } = balances[0];
   const { votingPower } = votingPowers[0];
   const { quotePrice } = dieselPools[0];
-  const { rewardPool, pendingClaims } = smtPool[0];
 
   const finalRshares = parseFloat(stake) + parseFloat(delegationsIn);
   const power = (votingPower * weight) / 10000;
@@ -32,7 +34,6 @@ exports.calculateHiveEngineVote = async ({
   const rshares = (power * finalRshares) / 10000;
   // we calculate price in hbd cent for usd multiply quotePrice hiveCurrency.usdCurrency
   const price = parseFloat(quotePrice) * hiveCurrency.price;
-  const rewards = parseFloat(rewardPool) / parseFloat(pendingClaims);
 
   const engineVotePrice = rshares * price * rewards;
   return { engineVotePrice, rshares, rewards };
