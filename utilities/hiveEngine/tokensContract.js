e39736e12ea7f9280afc24934ce4581a139c74a3
fesmofet <borees2@gmail.com>
2022-04-14 11:59:17 +0300

    engine proxy

--- utilities/hiveEngine/tokensContract.js
+++ utilities/hiveEngine/tokensContract.js
@@ -1,4 +1,4 @@
-const engineQuery = require('utilities/hiveEngine/engineQuery');
+const { engineProxy } = require('utilities/hiveEngine/engineQuery');
 
 /**
  * balances of the users
@@ -13,7 +13,7 @@ const engineQuery = require('utilities/hiveEngine/engineQuery');
  * pendingUndelegations = quantity of tokens being undelegated
  */
 
-exports.getTokenBalances = async ({ query }) => engineQuery({
+exports.getTokenBalances = async ({ query }) => engineProxy({
   params: {
     contract: 'tokens',
     table: 'balances',
