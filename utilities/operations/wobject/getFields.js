c2e2b52fff5800c874917ee8f649ab326c1ba471
MashaYakovenko <mariiay@wizardsdev.com>
2022-05-25 17:45:02 +0300

    fix on object fields

--- utilities/operations/wobject/getFields.js
+++ utilities/operations/wobject/getFields.js
@@ -1,5 +1,5 @@
 const _ = require('lodash');
-const { getWobjectFields, fillObjectByExposedFields } = require('../../helpers/wObjectHelper');
+const { getWobjectFields, fillObjectByExposedFields, calculateApprovePercent } = require('../../helpers/wObjectHelper');
 const ObjectTypeModel = require('../../../models/ObjectTypeModel');
 const { FIELDS_NAMES } = require('../../../constants/wobjectsData');
 
@@ -10,6 +10,10 @@ exports.getFields = async ({ authorPermlink }) => {
   const { objectType } = await ObjectTypeModel.getOne({ name: wobject.object_type });
   const exposedFields = _.get(objectType, 'exposedFields', Object.values(FIELDS_NAMES));
   const objectWithFields = await fillObjectByExposedFields(wobject, exposedFields);
+  for (const field of objectWithFields.fields) {
+    if (_.has(field, '_id')) field.createdAt = field._id.getTimestamp().valueOf();
+    field.approvePercent = calculateApprovePercent(field);
+  }
 
   return { fields: objectWithFields.fields };
 };
