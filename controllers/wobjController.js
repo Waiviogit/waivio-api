506408e152d8332afcbebeac10d5ac416979c9e6
MashaYakovenko <mariiay@wizardsdev.com>
2022-05-30 10:01:15 +0300

    companyId return

--- controllers/wobjController.js
+++ controllers/wobjController.js
@@ -45,7 +45,6 @@ const show = async (req, res, next) => {
 
   if (!value) return;
 
-  // проверить в мидлваре как обрабатываются филды!!!
   const { wobjectData, error } = await wobjectInfo.getOne(value);
 
   if (error) return next(error);
