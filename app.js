6cd36152bda6b4cdcfbc0271b80911cae27c9864
fesmofet <borees2@gmail.com>
2022-06-22 23:07:41 +0300

    try no cache

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const Sentry = require('@sentry/node');
 const swaggerUi = require('swagger-ui-express');
 const bodyParser = require('body-parser');
 const nocache = require('nocache');
-const { createNamespace, destroyNamespace } = require('cls-hooked');
+const { createNamespace } = require('cls-hooked');
 const { routes } = require('routes');
 const {
   moderateWobjects, checkUserFollowers, fillPostAdditionalInfo, siteUserStatistics,
@@ -101,7 +101,6 @@ app.use(Sentry.Handlers.errorHandler({
 app.use((req, res, next) => {
   res.on('close', () => {
     processHelper.responseOnClose({ session });
-    destroyNamespace('request-session');
   });
   res.status(res.result.status || 200).json(res.result.json);
 });
@@ -110,7 +109,6 @@ app.use((req, res, next) => {
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
-  destroyNamespace('request-session');
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   // render the error page
