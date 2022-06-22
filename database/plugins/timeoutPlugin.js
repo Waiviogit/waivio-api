df6639881870ac2161854a4ec6ab8a2a6021db81
fesmofet <borees2@gmail.com>
2022-06-22 21:54:47 +0300

    cursor timeoutPlugin.js

--- database/plugins/timeoutPlugin.js
+++ database/plugins/timeoutPlugin.js
@@ -3,7 +3,6 @@ const { CURSOR_TIMEOUT } = require('../../constants/common');
 function cursorTimeout(schema) {
   schema.pre('find', setCursorTimeout);
   schema.pre('findOne', setCursorTimeout);
-  schema.pre('aggregate', setCursorTimeout);
 }
 
 function setCursorTimeout() {
