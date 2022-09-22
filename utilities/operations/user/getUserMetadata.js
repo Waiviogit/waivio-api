9b79e91d42a1294c973843e279b7fd6ac4df05ed
fesmofet <borees2@gmail.com>
2022-09-22 11:26:57 +0300

    rewrite authors

--- utilities/operations/user/getUserMetadata.js
+++ utilities/operations/user/getUserMetadata.js
@@ -1,7 +1,7 @@
 const { User } = require('models');
 
 module.exports = async (userName) => {
-  const { user, error } = await User.getOne(userName, ['+user_metadata', '+privateEmail']);
+  const { user, error } = await User.getOne(userName, { user_metadata: 1, privateEmail: 1 });
   if (error || !user) return { error: error || { status: 404, message: 'User not Found!' } };
   return { user_metadata: user.user_metadata, privateEmail: user.privateEmail };
 };
