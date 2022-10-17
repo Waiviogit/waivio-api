f256a26a2b0cf3b39d26bf2cd69e50c685c25b54
fesmofet <borees2@gmail.com>
2022-10-17 17:02:28 +0300

    lowercase on api

--- controllers/validators/departments.js
+++ controllers/validators/departments.js
@@ -1,9 +1,9 @@
 const Joi = require('joi');
 
-const options = { allowUnknown: true, stripUnknown: true };
+const options = { allowUnknown: true, stripUnknown: true, convert: true };
 
 exports.departmentsSchema = Joi.object().keys({
-  name: Joi.string(),
-  names: Joi.array().items(Joi.string()),
-  excluded: Joi.array().items(Joi.string()),
+  name: Joi.string().lowercase(),
+  names: Joi.array().items(Joi.string().lowercase()),
+  excluded: Joi.array().items(Joi.string().lowercase()),
 }).without('name', 'names').options(options);
