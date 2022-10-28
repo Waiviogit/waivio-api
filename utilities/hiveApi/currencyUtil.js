bf47e0853e389cd206159ddfa01874a8795c1c05
fesmofet <borees2@gmail.com>
2022-10-28 14:20:32 +0300

    fix_comments_crash

--- utilities/hiveApi/currencyUtil.js
+++ utilities/hiveApi/currencyUtil.js
@@ -1,11 +1,9 @@
-const { getClient } = require('utilities/hiveApi/clientOptions');
-const { REDIS_KEYS } = require('constants/common');
+const { currencyClient } = require('utilities/hiveApi/hiveClient');
 
 exports.getRewardFund = async () => {
   try {
-    const client = await getClient(REDIS_KEYS.TEST_LOAD.POST);
     return {
-      result: await client.call('condenser_api', 'get_reward_fund', ['post']),
+      result: await currencyClient.call('condenser_api', 'get_reward_fund', ['post']),
     };
   } catch (error) {
     return { error };
@@ -14,9 +12,8 @@ exports.getRewardFund = async () => {
 
 exports.getCurrentMedianHistoryPrice = async () => {
   try {
-    const client = await getClient(REDIS_KEYS.TEST_LOAD.POST);
     return {
-      result: await client.call('condenser_api', 'get_current_median_history_price', []),
+      result: await currencyClient.call('condenser_api', 'get_current_median_history_price', []),
     };
   } catch (error) {
     return { error };
