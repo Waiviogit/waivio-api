6ea13882ebd8a8ea86e6fe73439f79bc23e27012
fesmofet <borees2@gmail.com>
2022-12-06 16:03:13 +0200

    departments_by_wobject

--- controllers/departmentController.js
+++ controllers/departmentController.js
@@ -4,11 +4,31 @@ const validators = require('./validators');
 const getDepartments = async (req, res, next) => {
   const value = validators.validate(req.body, validators.departments.departmentsSchema, next);
   if (!value) return;
-  const { result, error } = await departments.getDepartments(value);
+  const {
+    result,
+    error,
+  } = await departments.getDepartments(value);
   if (error) return next(error);
   res.json(result);
 };
 
+const getWobjectsByDepartments = async (req, res, next) => {
+  const value = validators
+    .validate(req.body, validators.departments.departmentsWobjectsSchema, next);
+  if (!value) return;
+  const {
+    wobjects,
+    hasMore,
+    error,
+  } = await departments.getWobjectsByDepartments(value);
+
+  if (error) return next(error);
+
+  res.result = { status: 200, json: { wobjects, hasMore } };
+  next();
+};
+
 module.exports = {
   getDepartments,
+  getWobjectsByDepartments,
 };
