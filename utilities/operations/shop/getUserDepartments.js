7b3f4b917005c771663d704a96fde13280a95964
fesmofet <borees2@gmail.com>
2023-02-23 14:20:09 +0200

    fix/shop

--- utilities/operations/shop/getUserDepartments.js
+++ utilities/operations/shop/getUserDepartments.js
@@ -1,18 +1,27 @@
 const { Wobj, Post, Department } = require('models');
 const { OBJECT_TYPES } = require('constants/wobjectsData');
 const _ = require('lodash');
+const { User } = require('../../../models');
+const { SELECT_USER_CAMPAIGN_SHOP } = require('../../../constants/usersData');
 
 exports.getTopDepartments = async ({
-  userName, wobjectsFromPosts, skip = 0, limit = 10,
+  userName,
+  wobjectsFromPosts,
+  skip = 0,
+  limit = 10,
+  user,
 }) => {
+  if (!user) ({ user } = await User.getOne(userName, SELECT_USER_CAMPAIGN_SHOP));
+  const hideLinkedObjects = _.get(user, 'user_metadata.settings.shop.hideLinkedObjects', false);
   if (!wobjectsFromPosts) {
     wobjectsFromPosts = await Post.getProductLinksFromPosts({ userName });
   }
+
   const orFilter = [
     { 'authority.ownership': userName },
     { 'authority.administrative': userName },
   ];
-  if (!_.isEmpty(wobjectsFromPosts)) {
+  if (!_.isEmpty(wobjectsFromPosts) && !hideLinkedObjects) {
     orFilter.push({ author_permlink: { $in: wobjectsFromPosts } });
   }
 
