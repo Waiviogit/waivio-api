7b4e55cfc7b9cc5f08d56460e4cf63dfb3048f3a
fesmofet <borees2@gmail.com>
2023-03-01 16:32:23 +0200

    fix/arrow shop

--- utilities/helpers/shopHelper.js
+++ utilities/helpers/shopHelper.js
@@ -114,11 +114,16 @@ const subdirectoryMap = ({ filteredDepartments, allDepartments }) => _
   .map(filteredDepartments, (department) => {
     const subdirectories = _.filter(
       allDepartments,
-      (d) => _.includes(d.related, department.name)
-        && d.objectsCount < department.objectsCount
-        && d.objectsCount > 10,
-    );
+      (d) => {
+        const mainCondition = _.includes(d.related, department.name)
+          && d.objectsCount < department.objectsCount
+          && d.objectsCount > 1;
 
+        return _.isEmpty(filteredDepartments)
+          ? mainCondition
+          : mainCondition && !_.includes(_.map(filteredDepartments, 'name'), d.name);
+      },
+    );
     const subdirectoriesCondition = subdirectories.length > 1;
 
     return {
