2efaac10936bc3d7202b0662f2385e7ebcfac197
fesmofet <borees2@gmail.com>
2023-03-07 16:02:14 +0200

    5050_Uncategorized_main_shop

--- utilities/operations/shop/getShopDepartments.js
+++ utilities/operations/shop/getShopDepartments.js
@@ -1,9 +1,10 @@
-const { Department } = require('models');
+const { Department, Wobj } = require('models');
 const shopHelper = require('utilities/helpers/shopHelper');
 const _ = require('lodash');
 const {
   filterDepartments, mapDepartments, getDepartmentsOnWobject,
 } = require('utilities/operations/departments/departmentsMapper');
+const { UNCATEGORIZED_DEPARTMENT } = require('constants/departments');
 
 const makeConditions = ({ name, excluded = [] }) => {
   if (name) return { name: { $nin: [name, ...excluded] }, related: name };
@@ -19,10 +20,22 @@ module.exports = async ({ name, excluded = [] } = {}) => {
     },
   );
 
+  const { result: uncategorized } = await Wobj
+    .findOne({ $or: [{ departments: [] }, { departments: null }] });
+
   if (error) return { error };
   if (_.isEmpty(departments)) return { result: [] };
 
-  if (!name) return { result: await mapDepartments(departments) };
+  if (!name) {
+    const mappedDepartments = await mapDepartments(departments);
+    if (uncategorized) {
+      mappedDepartments.push({
+        name: UNCATEGORIZED_DEPARTMENT,
+        subdirectory: false,
+      });
+    }
+    return { result: mappedDepartments };
+  }
   if (name) {
     return {
       result: await mapDepartments(
