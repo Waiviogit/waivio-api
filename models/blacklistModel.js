b6c62e11cd2b86b563c82b5127ee51c38538a293
fesmofet <borees2@gmail.com>
2023-04-10 15:50:06 +0300

    5179_addDataToCampaignPosts

--- models/blacklistModel.js
+++ models/blacklistModel.js
@@ -7,3 +7,23 @@ exports.find = async (condition, select = {}) => {
     return { error };
   }
 };
+
+exports.findOne = async ({ filter, projection, options }) => {
+  try {
+    return { result: await Blacklist.findOne(filter, projection, options).lean() };
+  } catch (error) {
+    return { error };
+  }
+};
+
+exports.getUserBlacklist = async (user = '') => {
+  const { result, error } = await this.findOne({ filter: { user } });
+  if (!result || error) return [];
+
+  return [
+    ...new Set([
+      ...result?.blackList ?? [],
+      ...(result?.followLists ?? []).map((fl) => fl.blackList).flat(1),
+    ]),
+  ];
+};
