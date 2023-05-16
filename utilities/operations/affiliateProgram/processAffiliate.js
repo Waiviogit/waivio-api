daa84292020c9f69eeb40096feaef05b9ea94075
fesmofet <borees2@gmail.com>
2023-05-16 09:45:54 +0300

    start affiliate

---
+++ utilities/operations/affiliateProgram/processAffiliate.js
@@ -0,0 +1,7 @@
+const processAppAffiliate = async ({ countryCode = 'US', app }) => {
+  // wor waivio condition authority === 'waivio'
+  // else owner and admins
+
+};
+
+module.exports = processAppAffiliate;
