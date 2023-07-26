ba02ce5428b677e7947be205a6df8ffdbf3f37e4
fesmofet <borees2@gmail.com>
2023-07-26 15:20:22 +0300

    5647 cachePreview.js

--- routes/routes.js
+++ routes/routes.js
@@ -263,6 +263,9 @@ postRoutes.route('/post/like-post')
   .post(PostController.likePost);
 postRoutes.route('/posts/getMany')
   .post(PostController.getManyPosts);
+postRoutes.route('/posts/preview-cache')
+  .post(PostController.getPreviewLinks)
+  .put(PostController.putPreviewUrl);
 postRoutes.route('/posts')
   .post(PostController.getByCategory);
 postRoutes.route('/post_comments')
