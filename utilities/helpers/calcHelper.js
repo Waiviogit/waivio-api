8fd6c0c79d63366d294fe85aaa0166c484b35e96
fesmofet <borees2@gmail.com>
2023-09-20 14:01:38 +0300

    5905

--- utilities/helpers/calcHelper.js
+++ utilities/helpers/calcHelper.js
@@ -5,7 +5,7 @@ exports.add = (...args) => _.reduce(args, (value, element) => (
   new BigNumber(value).plus(element)), new BigNumber(0)).toNumber();
 
 exports.roundToEven = (num) => {
-  let rounded = Math.round(num);
+  let rounded = Math.ceil(num);
   if (rounded % 2 !== 0) { // if the rounded value is odd
     rounded += (num > 0) ? 1 : -1; // add or subtract 1 to make it even
   }
