eeca75e2bfef21df0769deba30fc50ccfa49c229
fesmofet <borees2@gmail.com>
2023-09-27 14:09:20 +0300

    4041_webPage

--- constants/wobjectsData.js
+++ constants/wobjectsData.js
@@ -62,7 +62,7 @@ exports.FIELDS_NAMES = {
   AFFILIATE_GEO_AREA: 'affiliateGeoArea',
   AFFILIATE_URL_TEMPLATE: 'affiliateUrlTemplate',
   AFFILIATE_CODE: 'affiliateCode',
-  WEB_PAGE: 'webPage',
+  WEB_PAGE: 'webpage',
 };
 
 exports.LIST_TYPES = {
@@ -100,7 +100,7 @@ exports.OBJECT_TYPES = {
   NEWS_FEED: 'newsfeed',
   SHOP: 'shop',
   AFFILIATE: 'affiliate',
-  WEB_PAGE: 'webPage',
+  WEB_PAGE: 'webpage',
 };
 
 exports.ADMIN_ROLES = {
