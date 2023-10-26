3dd8f7e9226fa8f1f19386e635589885da69fcaa
fesmofet <borees2@gmail.com>
2023-10-26 14:49:29 +0300

    5708 ttl main -1 day

--- utilities/operations/shop/mainShop/getShopDepartments.js
+++ utilities/operations/shop/mainShop/getShopDepartments.js
@@ -23,13 +23,13 @@ const makeConditions = ({ name, excluded = [], path = [] }) => {
 
 // we can add host in future for sites
 module.exports = async ({ name, excluded = [], path = [] } = {}) => {
-  // const key = `${CACHE_KEY.MAIN_SHOP_DEPARTMENTS}:${getCacheKey({
-  //   name, path, excluded,
-  // })}`;
-  // const cache = await getCachedData(key);
-  // if (cache) {
-  //   return jsonHelper.parseJson(cache, { result: [] });
-  // }
+  const key = `${CACHE_KEY.MAIN_SHOP_DEPARTMENTS}:${getCacheKey({
+    name, path, excluded,
+  })}`;
+  const cache = await getCachedData(key);
+  if (cache) {
+    return jsonHelper.parseJson(cache, { result: [] });
+  }
 
   const { result: departments, error } = await Department.find(
     {
@@ -38,13 +38,13 @@ module.exports = async ({ name, excluded = [], path = [] } = {}) => {
     },
   );
 
-  const { result: uncategorized } = await Wobj
-    .findOne({ departments: { $in: [null, []] } });
-
   if (error) return { error };
   if (_.isEmpty(departments)) return { result: [] };
 
   if (!name) {
+    const { result: uncategorized } = await Wobj
+      .findOne({ departments: { $in: [null, []] } });
+
     const mappedDepartments = await mapDepartments(departments);
     if (uncategorized) {
       mappedDepartments.push({
@@ -54,38 +54,27 @@ module.exports = async ({ name, excluded = [], path = [] } = {}) => {
     }
     const result = shopHelper.omitRelated(mappedDepartments);
 
-    // await setCachedData({
-    //   key,
-    //   data: { result },
-    //   ttl: TTL_TIME.THIRTY_MINUTES,
-    // });
-    return { result };
-  }
-  if (name) {
-    const mappedDepartments = await mapDepartments(
-      await filterDepartments(departments, [...excluded, name], path),
-      [...excluded, name],
-      path,
-    );
-    const result = shopHelper.omitRelated(
-      shopHelper.orderBySubdirectory(mappedDepartments),
-    );
-
-    // await setCachedData({
-    //   key,
-    //   data: { result },
-    //   ttl: TTL_TIME.THIRTY_MINUTES,
-    // });
+    await setCachedData({
+      key,
+      data: { result },
+      ttl: TTL_TIME.ONE_DAY,
+    });
     return { result };
   }
 
-  const filtered = await getDepartmentsOnWobject(departments);
-  const result = shopHelper.omitRelated(
-    shopHelper.orderBySubdirectory(filtered),
+  const mappedDepartments = await mapDepartments(
+    await filterDepartments(departments, [...excluded, name], path),
+    [...excluded, name],
+    path,
   );
-  // await setCachedData({
-  //   key, data: { result }, ttl: TTL_TIME.THIRTY_MINUTES,
-  // });
+  const result = shopHelper.omitRelated(
+    shopHelper.orderBySubdirectory(mappedDepartments),
+  );
 
+  await setCachedData({
+    key,
+    data: { result },
+    ttl: TTL_TIME.THIRTY_MINUTES,
+  });
   return { result };
 };
