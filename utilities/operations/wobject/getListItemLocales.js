0039164448cc025fd78edb8d794b2925d0107cd4
fesmofet <borees2@gmail.com>
2023-11-10 13:17:06 +0200

    6116_listLocale

---
+++ utilities/operations/wobject/getListItemLocales.js
@@ -0,0 +1,17 @@
+const { Wobj } = require('models');
+const { FIELDS_NAMES } = require('../../../constants/wobjectsData');
+
+const getListItemLocales = async ({ authorPermlink, itemLink }) => {
+  const { wObject } = await Wobj.getOne(authorPermlink);
+
+  if (!wObject) {
+    return [];
+  }
+
+  return (wObject?.fields ?? [])
+    .filter((el) => el.name === FIELDS_NAMES.LIST_ITEM && el.body === itemLink)
+    .map((el) => el.locale)
+    .filter((el, index, self) => index === self.indexOf(el));
+};
+
+module.exports = getListItemLocales;
