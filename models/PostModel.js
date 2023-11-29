7c2372746809927e3ba4c39f233638c0c3604e48
fesmofet <borees2@gmail.com>
2023-11-29 13:48:28 +0200

    6175 feed

--- models/PostModel.js
+++ models/PostModel.js
@@ -67,12 +67,13 @@ exports.getByFollowLists = async ({
   const pipe = [
     {
       $match: {
-        $or: [{ author: { $in: users } }, { 'wobjects.author_permlink': { $in: authorPermlinks } }],
-        ...getBlockedAppCond(),
-        ...(_.get(filtersData, 'require_wobjects') && { 'wobjects.author_permlink': { $in: [...filtersData.require_wobjects] } }),
-        ...(!_.isEmpty(authorPermlinks) && { language: { $in: userLanguages } }),
-        ...(!_.isEmpty(hiddenPosts) && { _id: { $nin: hiddenPosts } }),
-        ...(!_.isEmpty(muted) && { author: { $nin: muted }, 'reblog_to.author': { $nin: muted } }),
+        author: { $in: users },
+        // $or: [{ author: { $in: users } }, { 'wobjects.author_permlink': { $in: authorPermlinks } }],
+        // ...getBlockedAppCond(),
+        //     ...(_.get(filtersData, 'require_wobjects') && { 'wobjects.author_permlink': { $in: [...filtersData.require_wobjects] } }),
+        // ...(!_.isEmpty(authorPermlinks) && { language: { $in: userLanguages } }),
+        // ...(!_.isEmpty(hiddenPosts) && { _id: { $nin: hiddenPosts } }),
+        // ...(!_.isEmpty(muted) && { author: { $nin: muted }, 'reblog_to.author': { $nin: muted } }),
       },
     },
     { $sort: { _id: -1 } },
