a90f6350e831e24b3f8594d97313889a2d279e44
fesmofet <borees2@gmail.com>
2023-12-07 17:11:56 +0200

    fix/threads_hasmore

--- controllers/threadsController.js
+++ controllers/threadsController.js
@@ -4,25 +4,25 @@ const threads = require('../utilities/operations/threads');
 const byHashtag = async (req, res, next) => {
   const value = validators.validate(req.query, validators.threads.byHashtagSchema, next);
   if (!value) return;
-  const { result, error } = await threads.getThreads.byHashtag(value);
+  const { result, hasMore, error } = await threads.getThreads.byHashtag(value);
   if (error) return next(error);
-  res.json(result);
+  res.json({ result, hasMore });
 };
 
 const byUser = async (req, res, next) => {
   const value = validators.validate(req.query, validators.threads.byUserSchema, next);
   if (!value) return;
-  const { result, error } = await threads.getThreads.byUser(value);
+  const { result, hasMore, error } = await threads.getThreads.byUser(value);
   if (error) return next(error);
-  res.json(result);
+  res.json({ result, hasMore });
 };
 
 const hashtagsCount = async (req, res, next) => {
   const value = validators.validate(req.query, validators.threads.hashtagCountSchema, next);
   if (!value) return;
-  const { result, error } = await threads.getHashtags.getTrendingHashTagsWithCount(value);
+  const { result, hasMore, error } = await threads.getHashtags.getTrendingHashTagsWithCount(value);
   if (error) return next(error);
-  res.json(result);
+  res.json({ result, hasMore });
 };
 
 module.exports = {
