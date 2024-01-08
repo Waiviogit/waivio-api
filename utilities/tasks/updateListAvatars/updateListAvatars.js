0dfebd229485861148e86dfa3e6707005afe6927
fesmofet <borees2@gmail.com>
2024-01-08 15:27:29 +0200

    aws V3 upgrade

--- utilities/tasks/updateListAvatars/updateListAvatars.js
+++ utilities/tasks/updateListAvatars/updateListAvatars.js
@@ -1,8 +1,5 @@
 const { base64ByUrl } = require('utilities/helpers/imagesHelper');
 const image = require('utilities/images/image');
-const AWS = require('aws-sdk');
-const { AWSS3_IMAGE_PARAMS } = require('constants/common');
-const util = require('util');
 const { WObject } = require('database').models;
 
 const extractIdFromUrl = (url) => {
@@ -14,50 +11,6 @@ const extractIdFromUrl = (url) => {
   }
 
   return null;
-};
-
-const updateAllImages = async () => {
-  const spacesEndpoint = new AWS.Endpoint(process.env.AWS_ENDPOINT);
-
-  const _s3 = new AWS.S3({
-    endpoint: spacesEndpoint,
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    params: AWSS3_IMAGE_PARAMS,
-  });
-  const listObjectsV2Async = util.promisify(_s3.listObjectsV2.bind(_s3));
-  const listObjectsAsync = async (StartAfter = '') => {
-    try {
-      const data = await listObjectsV2Async({ StartAfter });
-      return { result: data?.Contents ?? [] };
-    } catch (error) {
-      return { error };
-    }
-  };
-
-  let result = [];
-  let startAfter = '';
-  do {
-    ({ result } = await listObjectsAsync(startAfter));
-    startAfter = result[result.length - 1]?.Key;
-    for (const resultElement of result) {
-      const imageUrl = `https://waivio.nyc3.digitaloceanspaces.com/${resultElement?.Key}`;
-      const imageId = extractIdFromUrl(imageUrl);
-      if (!imageId) {
-        console.log(imageUrl, 'error');
-        continue;
-      }
-      const buffer = await base64ByUrl(imageUrl);
-      if (!buffer) {
-        console.log(imageUrl, 'buffer error');
-      }
-
-      await image.uploadInS3(buffer, imageId);
-      console.log(imageUrl, 'updated');
-    }
-  } while (startAfter);
-
-  console.log('task finished');
 };
 
 const updateListAvatars = async () => {
