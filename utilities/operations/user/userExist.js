14765c240d78c8a878c0620db27764df54bc170a
fesmofet <borees2@gmail.com>
2024-02-08 11:19:01 +0200

    hiveUserExists

---
+++ utilities/operations/user/userExist.js
@@ -0,0 +1,13 @@
+const { User } = require('models');
+
+const hiveUserExist = async ({ userName = '' }) => {
+  if (userName.includes('_')) return { result: false };
+
+  const { user } = await User.getOne(userName, 'name');
+
+  return { result: !!user };
+};
+
+module.exports = {
+  hiveUserExist,
+};
