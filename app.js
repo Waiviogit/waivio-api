9cf677f877fd40e3ae604de0b85cb4501b9cb99d
fesmofet <borees2@gmail.com>
2024-06-03 11:16:05 +0300

    log 500

--- app.js
+++ app.js
@@ -118,6 +118,7 @@ app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   // render the error page
+  console.log(err);
   res.status(err.status || 500).json({ message: err.message });
 });
 
