d303cbb585665096f1969aac75725e2929ad619a
fesmofet <borees2@gmail.com>
2024-06-05 10:56:50 +0300

    search/add-search-param

--- utilities/operations/search/searchWobjects.js
+++ utilities/operations/search/searchWobjects.js
@@ -71,6 +71,7 @@ const defaultWobjectSearch = async (data) => {
     pipeline: makePipeline(data),
     string: data.string,
     object_type: data.object_type,
+    onlyObjectTypes: data.onlyObjectTypes,
   });
 
   if (data.needCounters && !error) {
@@ -130,9 +131,11 @@ const getSiteWobjects = async (data) => {
   });
 };
 
-const getWobjectsFromAggregation = async ({ pipeline, string, object_type }) => {
+const getWobjectsFromAggregation = async ({
+  pipeline, string, object_type, onlyObjectTypes,
+}) => {
   const { wobjects = [], error } = await Wobj.fromAggregation(pipeline);
-  const { wObject } = await Wobj.getOne(string, object_type, true);
+  const { wObject } = await Wobj.getOne(string, object_type, true, onlyObjectTypes);
 
   if (wObject && wobjects.length) {
     _.remove(wobjects, (wobj) => wObject.author_permlink === wobj.author_permlink);
