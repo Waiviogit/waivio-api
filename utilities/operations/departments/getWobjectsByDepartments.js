7fc70c2cbdb4aec06bce56c65c36ac3091fdc9c0
fesmofet <borees2@gmail.com>
2024-09-24 10:41:07 +0300

    6981-departments

--- utilities/operations/departments/getWobjectsByDepartments.js
+++ utilities/operations/departments/getWobjectsByDepartments.js
@@ -4,21 +4,32 @@ const shopHelper = require('utilities/helpers/shopHelper');
 const _ = require('lodash');
 const campaignsV2Helper = require('utilities/helpers/campaignsV2Helper');
 const { SELECT_USER_CAMPAIGN_SHOP } = require('constants/usersData');
+const { SHOP_SCHEMA } = require('../../../constants/shop');
 
 module.exports = async ({
-  departments, skip, limit, userName = '',
+  departments, skip, limit, userName = '', schema,
 }) => {
-  const { wobjects: result, error } = await Wobj.fromAggregation([
+  const pipe = [
     {
       $match: {
         departments: { $in: departments },
         'status.title': { $nin: REMOVE_OBJ_STATUSES },
       },
     },
-    ...shopHelper.getDefaultGroupStage(),
+  ];
+
+  if (schema === SHOP_SCHEMA.SHOP) {
+    pipe.push(...shopHelper.getDefaultGroupStage());
+  } else {
+    pipe.push({ $sort: { weight: -1, createdAt: -1 } });
+  }
+
+  pipe.push(
     { $skip: skip },
     { $limit: limit + 1 },
-  ]);
+  );
+
+  const { wobjects: result, error } = await Wobj.fromAggregation(pipe);
   if (error) return { error };
 
   const { user } = await User.getOne(userName, SELECT_USER_CAMPAIGN_SHOP);
