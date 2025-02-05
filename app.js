49f1362792ac7377fc7089b0841636588ef1b406
fesmofet <borees2@gmail.com>
2025-02-05 17:43:34 +0200

    remove logs

--- app.js
+++ app.js
@@ -50,7 +50,6 @@ app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   // render the error page
-  console.log(err?.stack);
   res.status(err.status || 500).json({ message: err.message });
 });
 
