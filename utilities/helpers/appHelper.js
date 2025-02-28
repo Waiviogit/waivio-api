7ebc284401dac4c4d7e507e3d4518b33813ffa8e
fesmofet <borees2@gmail.com>
2025-02-28 14:58:14 +0200

    7298-exclude-owner

--- utilities/helpers/appHelper.js
+++ utilities/helpers/appHelper.js
@@ -2,16 +2,12 @@ const { SHOP_SETTINGS_TYPE } = require('../../constants/sitesConstants');
 
 const getAppAuthorities = (app) => {
   const userShop = app?.configuration?.shopSettings?.type === SHOP_SETTINGS_TYPE.USER;
-
   const authorities = [...app.authority];
-
   if (userShop) {
     authorities.push(app?.configuration?.shopSettings?.value);
     return authorities;
   }
-
-  authorities.push(app.owner);
-
+  if (!app.disableOwnerAuthority) authorities.push(app.owner);
   return authorities;
 };
 
