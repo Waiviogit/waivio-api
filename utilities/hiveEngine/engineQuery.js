560b4662b473cc9881fb566a1c36d33860ddf2d7
fesmofet <borees2@gmail.com>
2025-02-28 22:04:37 +0200

    remove engine node

--- utilities/hiveEngine/engineQuery.js
+++ utilities/hiveEngine/engineQuery.js
@@ -32,7 +32,7 @@ exports.engineQuery = async ({
 };
 
 exports.engineProxy = async ({
-  hostUrl = 'https://engine.waivio.com',
+  hostUrl = _.sample(HIVE_ENGINE_NODES),
   method,
   params,
   endpoint,
