512eebfdbddd28303930ab6b2a187ab70067a538
fesmofet <borees2@gmail.com>
2025-06-25 17:02:20 +0300

    rewrite task

--- utilities/tasks/ratingHostsToImport/ratingHostsToImport.js
+++ utilities/tasks/ratingHostsToImport/ratingHostsToImport.js
@@ -40,23 +40,19 @@ const PageImportSchema = new mongoose.Schema({
   timestamps: false, versionKey: false,
 });
 
-const PAGES_LIMIT = 1000;
-
-const ratingHostsToImport = async (rank = 8) => {
+const ratingHostsToImport = async (rank = 6) => {
   try {
     const connection = await mongoose.connect('mongodb://localhost:27017/page_rank');
     const PageRank = connection.model('PageRank', PageRankSchema);
     const PageImport = connection.model('PageImport', PageImportSchema);
 
-    while (true) {
-      const pages = await PageRank.find({
-        page_rank_decimal: { $gte: Number(rank) },
-      }).sort({ _id: 1 }).limit(PAGES_LIMIT).lean();
-      if (pages.length === 0) break;
+    const pages = await PageRank.find({
+      page_rank_decimal: { $gte: Number(rank) },
+    }).sort({ _id: 1 }).lean();
+    if (pages.length === 0) return;
 
-      const mappedObjects = pages.map((el) => ({ name: el.domain, fieldUrl: `https://${el.domain}`, fieldRating: el.page_rank_decimal / 2 }));
-      await PageImport.insertMany(mappedObjects);
-    }
+    const mappedObjects = pages.map((el) => ({ name: el.domain, fieldUrl: `https://${el.domain}`, fieldRating: 5 }));
+    await PageImport.insertMany(mappedObjects);
   } catch (error) {
     console.error(error);
   }
