7665e759f229182aea79803c374efff3c830f838
fesmofet <borees2@gmail.com>
2025-11-04 14:16:16 +0200

    4523

--- utilities/helpers/shopHelper.js
+++ utilities/helpers/shopHelper.js
@@ -23,19 +23,21 @@ const makeFilterCondition = (filter = {}) => {
   if (_.get(filter, FIELDS_NAMES.TAG_CATEGORY)) {
     const condition = [];
     for (const category of filter.tagCategory) {
-      condition.push({
-        fields: {
-          $elemMatch: {
-            name: FIELDS_NAMES.CATEGORY_ITEM,
-            body: { $in: category.tags },
-            tagCategory: category.categoryName,
-            weight: { $gte: 0 },
+      for (const categoryElement of category.tags) {
+        condition.push({
+          fields: {
+            $elemMatch: {
+              name: FIELDS_NAMES.CATEGORY_ITEM,
+              body: categoryElement,
+              tagCategory: category.categoryName,
+              weight: { $gte: 0 },
+            },
           },
-        },
-      });
+        });
+      }
     }
 
-    if (condition.length) result.$or = condition;
+    if (condition.length) result.$and = condition;
   }
   if (filter.rating) {
     result.fields = { $elemMatch: { average_rating_weight: { $gte: filter.rating } } };
