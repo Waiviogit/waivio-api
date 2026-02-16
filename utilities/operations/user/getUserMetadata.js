e91acd5f7ae29bcd6ea31ef54b531f00fe126263
fesmofet <borees2@gmail.com>
2026-02-16 11:04:40 +0200

    add muted to user metadata

--- utilities/operations/user/getUserMetadata.js
+++ utilities/operations/user/getUserMetadata.js
@@ -32,25 +32,11 @@ module.exports = async ({
   userName,
   app,
 }) => {
-  const {
-    user,
-    error,
-  } = await User.getOne(userName, {
-    user_metadata: 1,
-    privateEmail: 1,
-  });
+  const { user, error } = await User.getOne(userName, { user_metadata: 1, privateEmail: 1 });
   if (error || !user) {
-    return {
-      error: error || {
-        status: 404,
-        message: 'User not Found!',
-      },
-    };
+    return { error: error || { status: 404, message: 'User not Found!' } };
   }
-  const muted = await checkMuted({
-    userName,
-    app,
-  });
+  const muted = await checkMuted({ userName, app });
 
   return {
     user_metadata: user.user_metadata,
